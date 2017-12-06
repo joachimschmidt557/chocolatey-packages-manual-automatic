d8f2e3757e6a86a549f9428f43f15c97d8d5be50
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-12-06 21:23:04 +0800

    WIP

---
+++ manual/unfreez/tools/chocolateyinstall.ps1
@@ -0,0 +1,5 @@
+﻿Install-ChocolateyZipPackage -PackageName 'unfreez' `
+-Url 'http://www.whitsoftdev.com/files/unfreez.zip' `
+-UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)" `
+-Checksum "6B61293748A8500BF7DC073612715EE8D22DAE38B0DA25B7264A3007CACC209F" `
+-ChecksumType "sha256"
