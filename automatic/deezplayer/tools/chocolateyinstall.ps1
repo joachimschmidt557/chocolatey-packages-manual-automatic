9655db0049d1522f7a4d2ddf138fcb7661f7612a
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-12-10 11:39:18 +0800

    Add packages

---
+++ automatic/deezplayer/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/imanel/DeezPlayer/releases/download/v2.0.1/DeezPlayer.Setup-2.0.1.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'deezplayer*'
+
+  checksum      = '47BC9ED0B541FBEC5F8FC935668E3D9EB6243725D8CD106197E9E58E7AA59A26'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/ezblocker/ReadMe.md
@@ -0,0 +1,5 @@
+﻿# EzBlocker
+
+EZBlocker is a simple-to-use ad blocker/muter for Spotify. EZBlocker will attempt to block ads on Spotify from loading. If an ad does load, EZBlocker will mute Spotify until the ad is over.
+
+There are no fancy features, just pure ad blocking. When an ad is blocked, only Spotify is muted, so no other sounds will be affected. EZBlocker aims to be the most stable and reliable ad blocker for Spotify.
