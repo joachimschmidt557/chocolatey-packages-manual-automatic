2206c9a36a41f20396df685d9e07c91b7a479aa9
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-12-16 13:25:56 +0800

    Finish ring

--- automatic/ring/tools/chocolateyinstall.ps1
+++ automatic/ring/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'https://github.com/IrosTheBeggar/mStream/releases/download/v3.2.0/mStreamExpress-installer-v0.6.exe'
-$url64      = ''
-$checksum32 = '94D653498B4F93B14F12A55CA06154E19C540C9B276E5D163F1CF84FA078F97A'
-$checksum64 = 'DD9FD814CC44BC2FFA9B9E547B1A6CBB42B6BE7B9358542D3EE7F6E10B676423'
+$url32      = 'https://dl.ring.cx/windows/ring-windows-nightly-20171129113113~gitcf5bbff.exe'
+$url64      = 'https://dl.ring.cx/windows/ring-windows-nightly_x86_64-20171129113113~gitcf5bbff.exe'
+$checksum32 = '453f82d1fd09a59784134f41b7cc452b63016c94bd0e64981137acbab0288078'
+$checksum64 = '93d89b76ae8184501cc6a012cae5cbecd05cbf804a0e041fde2cbb0bf7a2c5fa'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
