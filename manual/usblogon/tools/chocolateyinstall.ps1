0b04d224946d6471903f368731e07bdd78422143
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-01-03 15:07:17 +0800

    WIP...

---
+++ manual/usblogon/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://quadsoft.org/download/USBLogonSetup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'quadsoft usblogon*'
+
+  checksum      = '8F2503F220CF7357D2F61A33894129C660C8F817D39E6441301C319C37172CCC'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
