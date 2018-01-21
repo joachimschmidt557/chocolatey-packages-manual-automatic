08c2fdbed4852af4c858c4a8c974547a59aa9736
Chocolatey <chocolatey@realdimensions.net>
2018-01-21 08:18:56 +0000

    AU: 1 updated - quodlibet.install [skip ci] https://gist.github.com/8a2f24b529db5159a7c189c3b4ccd9a4/926db61947b44691fc02f3bf1783e497e1826f79

--- automatic/quodlibet.install/tools/chocolateyinstall.ps1
+++ automatic/quodlibet.install/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/IrosTheBeggar/mStream/releases/download/v3.2.0/mStreamExpress-installer-v0.6.exe'
-$checksum   = ''
+$url        = 'https://github.com/quodlibet/quodlibet/releases/download/release-4.0.2/quodlibet-4.0.2-installer.exe'
+$checksum   = '588477831ce3f0ddbd812058050d31130ca3d1f468a91da5fed6cfa208320513'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
