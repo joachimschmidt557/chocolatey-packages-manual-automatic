a6e7da40a1ec6dfd190b9a3a61a9509d401d5c6e
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-01-21 16:51:18 +0800

    Add quodlibet portable

---
+++ automatic/quodlibet.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/quodlibet/quodlibet/releases/download/release-4.0.2/quodlibet-4.0.2-installer.exe'
+$checksum   = '588477831ce3f0ddbd812058050d31130ca3d1f468a91da5fed6cfa208320513'
+
+Get-ChocolateyWebFile -PackageName 'quodlibet.portable' -FileFullPath "$toolsDir\QuodLibet.exe" -Url $url -Checksum $checksum -ChecksumType 'sha256'
