119575e71d9382957fb6a8ea2a8b00d66f14f64e
Chocolatey <chocolatey@realdimensions.net>
2018-01-21 09:03:17 +0000

    AU: 1 updated - quodlibet.portable [skip ci] https://gist.github.com/8a2f24b529db5159a7c189c3b4ccd9a4/48ec7b8aebc166bfedbe9636316d9c7c0e1d7a62

--- automatic/quodlibet.portable/tools/chocolateyinstall.ps1
+++ automatic/quodlibet.portable/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/quodlibet/quodlibet/releases/download/release-4.0.2/quodlibet-4.0.2-installer.exe'
-$checksum   = '588477831ce3f0ddbd812058050d31130ca3d1f468a91da5fed6cfa208320513'
+$url        = 'https://github.com/quodlibet/quodlibet/releases/download/release-4.0.2/quodlibet-4.0.2-portable.exe'
+$checksum   = '4af6cc69573cae13bedea9207617f670cc83a6ef38467a33a0db39fd0e0ad406'
 
 Get-ChocolateyWebFile -PackageName 'quodlibet.portable' -FileFullPath "$toolsDir\QuodLibet.exe" -Url $url -Checksum $checksum -ChecksumType 'sha256'
