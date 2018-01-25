ce1bf051843518ceb1fb7831cb43aff569fa7108
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-01-25 10:51:52 +0800

    Add resonic

---
+++ automatic/resonic.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/quodlibet/quodlibet/releases/download/release-4.0.2/quodlibet-4.0.2-installer.exe'
+$checksum   = '588477831ce3f0ddbd812058050d31130ca3d1f468a91da5fed6cfa208320513'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'quod libet*'
+
+  checksum      = $checksum
+  checksumType  = 'sha256'
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
