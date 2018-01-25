dcb5c635d654d407c8b622f2a48ac7f1c36f1ee0
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-01-25 17:50:57 +0800

    WIP

--- automatic/fd/not_yet_update.ps1
+++ automatic/fd/not_yet_update.ps1
@@ -1,6 +1,6 @@
 import-module au
 
-$releases = 'https://github.com/upx/upx/releases'
+$releases = 'https://github.com/sharkdp/fd/releases'
 
 function global:au_SearchReplace {
     @{
@@ -14,12 +14,12 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
 
-    #upx394w.zip
-    $re  = "upx.+w.zip"
+    # fd-v6.2.0-i686-pc-windows-gnu.zip
+    $re  = "fd-v.+-i686-pc-windows-gnu.zip"
     $url = $download_page.links | ? href -match $re | select -First 1 -expand href
     $url = "https://github.com" + $url
 
-    $version = ($url -split '/' | select -last 1 -skip 1) -Replace 'v',''
+    $version = ($url -split '-' | select -last 1 -skip 4) -Replace 'v',''
 
     $Latest = @{ URL = $url; Version = $version }
     return $Latest
