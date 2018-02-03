171ec18eb98487a35a9b01e285be4772f4c3c90e
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-02-03 21:25:58 +0800

    Add white

---
+++ manual/white/tools/chocolateyinstall.ps1
@@ -0,0 +1,2 @@
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Get-ChocolateyWebFile -PackageName 'bigbyte' -FileFullPath "$toolsDir\BigByte.exe" -Url 'http://www.donationcoder.com/Software/Skrommel/BigByte/BigByte.exe' -Checksum '1470C2B1A11E1BEB10890A4789CE9D7A34ABDDE170A6E8CB8C308086741B20E8' -ChecksumType 'sha256'
