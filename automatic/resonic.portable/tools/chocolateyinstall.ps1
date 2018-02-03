320af30bcbd0d1b8bc5e5c03dcb48ca54df03ce7
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-02-03 11:51:22 +0800

    WIP resonic

---
+++ automatic/resonic.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://resonic.at/get/player'
+$checksum   = 'ba6dbd8e92e5a9b94712aa85a3f9089b5af4d6da00ce63e93b50c14c5a31d0a7'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
