d047682c8aa07f0c2e86736160625a3996120d3c
Chocolatey <chocolatey@realdimensions.net>
2018-02-14 06:03:46 +0000

    AU: 1 updated - fd [skip ci] https://gist.github.com/8a2f24b529db5159a7c189c3b4ccd9a4/91afebfa96590f4db5165f808b779eb2f8c7fc9b

--- automatic/fd/tools/chocolateyInstall.ps1
+++ automatic/fd/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'fd'
-$url32      = 'https://github.com/sharkdp/fd/releases/download/v6.2.0/fd-v6.2.0-i686-pc-windows-gnu.zip'
-$url64      = 'https://github.com/sharkdp/fd/releases/download/v6.2.0/fd-v6.2.0-x86_64-pc-windows-gnu.zip'
-$checksum32 = '2ce7d1e4a03d33913cbb27867924ba90468aa98bd2669c38a474d52a6a5eb443'
-$checksum64 = '58b46398b6f07455f3138332f470d90b4cdb11015789a8dc474e0d130c6dd889'
+$url32      = 'https://github.com/sharkdp/fd/releases/download/v6.3.0/fd-v6.3.0-i686-pc-windows-gnu.zip'
+$url64      = 'https://github.com/sharkdp/fd/releases/download/v6.3.0/fd-v6.3.0-x86_64-pc-windows-gnu.zip'
+$checksum32 = 'ab3686c842bfb64166c264ec55ed0d92ec14651689b82465d795cd9b8e5943f3'
+$checksum64 = 'feaa46631d71f860316a6cdb6613a80ae4b8c04b44578505a81f9c70794ffd7b'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url32" `
