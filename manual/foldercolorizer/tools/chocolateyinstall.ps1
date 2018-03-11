606fab24da4b856e7a9a607c008502ff17a0a494
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-03-11 13:13:24 +0800

    Migrate foldercolorizer for new version

---
+++ manual/foldercolorizer/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'foldercolorizer'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://softorino.com/media/utilities/FolderColorizerSetup.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+
+  softwareName  = 'folder colorizer*'
+  checksum      = '26B3E394C9164BC761F0C3950D380060B0F27772ECD1A835E2A5161F676B593F'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
