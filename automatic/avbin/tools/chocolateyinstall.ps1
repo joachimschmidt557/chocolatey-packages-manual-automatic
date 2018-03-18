d2dafb233fe6d8f7f192b815d626d47adc1b44e7
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-03-18 15:21:52 +0800

    Add avbin, fmedia, qpdf

---
+++ automatic/avbin/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/IrosTheBeggar/mStream/releases/download/v3.2.0/mStreamExpress-installer-v0.6.exe'
+$checksum   = '923B68A419FF86FF5F0471820C1A7A6BB952163E02A7BA5779B8EA32F40F8716'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'mstream*'
+
+  checksum      = $checksum
+  checksumType  = 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/qpdf/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'fd'
+$url32      = 'https://github.com/sharkdp/fd/releases/download/v6.3.0/fd-v6.3.0-i686-pc-windows-gnu.zip'
+$url64      = 'https://github.com/sharkdp/fd/releases/download/v6.3.0/fd-v6.3.0-x86_64-pc-windows-gnu.zip'
+$checksum32 = 'ab3686c842bfb64166c264ec55ed0d92ec14651689b82465d795cd9b8e5943f3'
+$checksum64 = 'feaa46631d71f860316a6cdb6613a80ae4b8c04b44578505a81f9c70794ffd7b'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url32" `
+                             -Url64Bit "$url64" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum32" `
+                             -ChecksumType "sha256" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "sha256" `
