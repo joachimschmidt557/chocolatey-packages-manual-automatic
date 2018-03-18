ddba3c4e98cfcd636ed969a403748bb891d20940
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-03-18 14:15:27 +0800

    WIP resonic meta package

---
+++ automatic/resonic/not_yet_update.ps1
@@ -0,0 +1,23 @@
+import-module au
+
+$download = 'https://resonic.at/get/player'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_file = Invoke-WebRequest -Uri $download -Method Head
+
+    $version = ($download_file.Headers["Content-Disposition"] -split ' ' | select -last 1 ).replace('.msi"', '')
+
+    $Latest = @{ URL = ($download); Version = $version }
+    return $Latest
+}
+
+update -ChecksumFor 32
