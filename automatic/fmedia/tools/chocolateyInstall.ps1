5d9229592ee6c57ac572662c24fe5927952f0cbd
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-03-24 14:07:49 +0800

    Fix fmedia

--- automatic/fmedia/tools/chocolateyInstall.ps1
+++ automatic/fmedia/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'fmedia'
-$url32      = 'https://github.com/sharkdp/fd/releases/download/v6.3.0/fd-v6.3.0-i686-pc-windows-gnu.zip'
-$url64      = 'https://github.com/sharkdp/fd/releases/download/v6.3.0/fd-v6.3.0-x86_64-pc-windows-gnu.zip'
-$checksum32 = 'ab3686c842bfb64166c264ec55ed0d92ec14651689b82465d795cd9b8e5943f3'
-$checksum64 = 'feaa46631d71f860316a6cdb6613a80ae4b8c04b44578505a81f9c70794ffd7b'
+$url32      = 'http://fmedia.firmdev.com/fmedia-0.34.1-win-x64.zip'
+$url64      = 'http://fmedia.firmdev.com/fmedia-0.34.1-win-x86.zip'
+$checksum32 = ''
+$checksum64 = ''
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url32" `
