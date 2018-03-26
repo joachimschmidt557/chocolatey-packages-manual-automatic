d3ceef53e4d0b065d71ac9198ed48ef74d61253c
Chocolatey <chocolatey@realdimensions.net>
2018-03-26 14:52:31 +0000

    AU: 2 updated - fd youtube-dl-gui [skip ci]

--- automatic/fd/tools/chocolateyInstall.ps1
+++ automatic/fd/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'fd'
-$url32      = 'https://github.com/sharkdp/fd/releases/download/v6.3.0/fd-v6.3.0-i686-pc-windows-gnu.zip'
-$url64      = 'https://github.com/sharkdp/fd/releases/download/v6.3.0/fd-v6.3.0-x86_64-pc-windows-gnu.zip'
-$checksum32 = 'ab3686c842bfb64166c264ec55ed0d92ec14651689b82465d795cd9b8e5943f3'
-$checksum64 = 'feaa46631d71f860316a6cdb6613a80ae4b8c04b44578505a81f9c70794ffd7b'
+$url32      = 'https://github.com/sharkdp/fd/releases/download/v7.0.0/fd-v7.0.0-i686-pc-windows-gnu.zip'
+$url64      = 'https://github.com/sharkdp/fd/releases/download/v7.0.0/fd-v7.0.0-x86_64-pc-windows-gnu.zip'
+$checksum32 = '864312a830a4a2e29d49aac1bb7c25ab55e223590406e79b8cbc5b16836e58e4'
+$checksum64 = '074e280b3399aa2792cb3ae1fe20afab5f0465d69a07a694dbfbbbea2908211d'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url32" `
