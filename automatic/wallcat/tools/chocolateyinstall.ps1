f7bb6245533509872333abe4bae7d834f17a1d3d
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-03-28 10:51:34 +0800

    [PUSH wallcat]

--- automatic/wallcat/tools/chocolateyinstall.ps1
+++ automatic/wallcat/tools/chocolateyinstall.ps1
@@ -4,4 +4,4 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://github.com/PaitoAnderson/WallcatWindows/releases/download/1.0.4/Wallcat.exe'
 $checksum   = '3e7d23a45986e60087b49adaed96568cc90efe6baa34616395e72c55c43dc3c2'
 
-Get-ChocolateyWebFile -PackageName 'quodlibet.portable' -FileFullPath "$toolsDir\QuodLibet.exe" -Url $url -Checksum $checksum -ChecksumType 'sha256'
+Get-ChocolateyWebFile -PackageName 'quodlibet.portable' -FileFullPath "$toolsDir\Wallcat.exe" -Url $url -Checksum $checksum -ChecksumType 'sha256'
