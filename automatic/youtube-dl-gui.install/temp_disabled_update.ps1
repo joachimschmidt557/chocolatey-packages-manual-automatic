cd355fb1926879b586010218a47e75dd7f4ad678
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-03-31 09:44:00 +0800

    WIP youtube-dl-gui.install

---
+++ automatic/youtube-dl-gui.install/temp_disabled_update.ps1
@@ -0,0 +1,27 @@
+import-module au
+
+$releases = 'https://github.com/MrS0m30n3/youtube-dl-gui/releases'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    # youtube-dl-gui-0.4-win-setup.zip
+    $re  = "youtube-dl-gui-.+-win-setup.zip"
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+    $version = ($url -split '-' | select -last 1 -Skip 2)
+
+    $Latest = @{ URL = ("https://github.com" + $url); Version = $version }
+    return $Latest
+}
+
+update -ChecksumFor 32
