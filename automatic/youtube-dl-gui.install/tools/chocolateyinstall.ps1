49271763de63e40ca6aed49ead037662793eb177
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-03-31 09:53:23 +0800

    [AU youtube-dl-gui.install:0.1]

--- automatic/youtube-dl-gui.install/tools/chocolateyinstall.ps1
+++ automatic/youtube-dl-gui.install/tools/chocolateyinstall.ps1
@@ -1,24 +1,12 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/MrS0m30n3/youtube-dl-gui/releases/download/0.4/youtube-dl-gui-0.4-win-setup.zip'
-$checksum   = '230690b25bd1c5bf7e396f905fa51c6760af93ad24a50ce2fb0eee5ffa9d7068'
-
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  url           = $url
-
-  checksum      = $checksum
-  checksumType  = 'sha256'
-}
-
-Install-ChocolateyZipPackage @packageArgs
+$checksum   = 'B0FC0A0ED763639873438B859A9172B4B0059D81360AD481AA4577DD597F790D'
 
 $installPackageArgs = @{
   packageName = $env:ChocolateyPackageName
   fileType = 'EXE'
-  File = Get-ChildItem -Recurse $toolsDir\*youtube-dlg-*.exe | Select-Object -First 1
+  File = Get-ChildItem -Recurse $toolsDir\youtube-dlg-*.exe | Select-Object -First 1
 
   softwareName = 'Youtube-DLG*'
 
