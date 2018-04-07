cb2e9023521ba059e67ad1f641ea53027609d138
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-04-07 12:33:09 +0800

    Finish avbin

--- manual/avbin/tools/chocolateyinstall.ps1
+++ manual/avbin/tools/chocolateyinstall.ps1
@@ -1,21 +1,27 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/IrosTheBeggar/mStream/releases/download/v3.2.0/mStreamExpress-installer-v0.6.exe'
-$checksum   = '923B68A419FF86FF5F0471820C1A7A6BB952163E02A7BA5779B8EA32F40F8716'
+$url        = 'https://github.com/downloads/AVbin/AVbin/AVbin10-win32.exe'
+$url64      = 'https://github.com/downloads/AVbin/AVbin/AVbin10-win64.exe'
+$checksum   = 'CC844CE7F8E6F21ABFD5FE6794DEC06F740FE81494FAC27C66099F1D1C336475'
+$checksum64 = '63A6F858C9B97B9CAECBF19724F60548F8C3D6BA6877B7EE31F914D99E5BF854'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
+  url64         = $url64
 
-  softwareName  = 'mstream*'
+  softwareName  = 'avbin*'
 
   checksum      = $checksum
   checksumType  = 'sha256'
 
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+
+  silentArgs   = '/S'
   validExitCodes= @(0)
 }
 
