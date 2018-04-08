b968cb0f97906e0b53aa06c4bdc81400fc655c55
Chocolatey <chocolatey@realdimensions.net>
2018-04-08 02:05:10 +0000

    AU: 3 updated - fmedia ring shotcut.install [skip ci]

--- automatic/fmedia/tools/chocolateyInstall.ps1
+++ automatic/fmedia/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'fmedia'
-$url32      = 'http://fmedia.firmdev.com/fmedia-0.34.1-win-x64.zip'
-$url64      = 'http://fmedia.firmdev.com/fmedia-0.34.1-win-x86.zip'
-$checksum32 = '9ff54905303070f15eeb757ac23824a35d0de999fcd5286c27fe209d200eaff1'
-$checksum64 = '9911a41bf7b9382bcd0db4dbd59b141d909e1039b93d8b3e1c2889ad32f5b8b2'
+$url32      = 'http://fmedia.firmdev.com/fmedia-0.35-win-x64.zip'
+$url64      = 'http://fmedia.firmdev.com/fmedia-0.35-win-x86.zip'
+$checksum32 = 'aafb35579388a698cbd71527c3e7416c1d17ad902b6dc0988ca37327fe709c59'
+$checksum64 = '669fe74806c26dc88fb5ef6ae1837db556ad62e94bc0777bd3ff2a15ca904aae'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url32" `
