b8157e5e6858695de1946d17bee35da8a5ab7ccc
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-08-07 09:06:30 +0800

    Update quodlibet.portable

--- automatic/quodlibet.portable/tools/chocolateyinstall.ps1
+++ automatic/quodlibet.portable/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/quodlibet/quodlibet/releases/download/release-4.0.2/quodlibet-4.0.2-portable.exe'
-$checksum   = '4af6cc69573cae13bedea9207617f670cc83a6ef38467a33a0db39fd0e0ad406'
+$url        = 'https://github.com/quodlibet/quodlibet/releases/download/release-4.1.0/quodlibet-4.1.0-portable.exe'
+$checksum   = '13C6007BB20329CACE4596651D09983F8C8CE5D37B2CAA4DC2837CAB4D088988'
 
 Get-ChocolateyWebFile -PackageName 'quodlibet.portable' -FileFullPath "$toolsDir\QuodLibet.exe" -Url $url -Checksum $checksum -ChecksumType 'sha256'
