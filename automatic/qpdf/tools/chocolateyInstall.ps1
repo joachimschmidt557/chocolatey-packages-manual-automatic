dd6f1fa90e487100deb6909e654520f966947fb1
Chocolatey <chocolatey@realdimensions.net>
2018-09-18 12:31:22 +0000

    AU: 3 updated - qpdf sharpkeys.portable vnote.portable [skip ci] https://gist.github.com/8a2f24b529db5159a7c189c3b4ccd9a4/0b506bf1dc0cf5083d77f1b23cce53a8d6581383

--- automatic/qpdf/tools/chocolateyInstall.ps1
+++ automatic/qpdf/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'qpdf'
-$url32      = 'https://github.com/qpdf/qpdf/releases/download/release-qpdf-8.0.2/qpdf-8.0.2-bin-mingw32.zip'
-$url64      = 'https://github.com/qpdf/qpdf/releases/download/release-qpdf-8.0.2/qpdf-8.0.2-bin-mingw64.zip'
-$checksum32 = 'be2b3be37958d5582ba9565d5818b2e5bc7f8a392d5bc6d4c624113534b66766'
-$checksum64 = 'c14420048e327899f006383b5a550f03fb3ccc7d2797155ec0d43846ba2ccea2'
+$url32      = 'https://github.com/qpdf/qpdf/releases/download/release-qpdf-8.2.1/qpdf-8.2.1-bin-mingw32.zip'
+$url64      = 'https://github.com/qpdf/qpdf/releases/download/release-qpdf-8.2.1/qpdf-8.2.1-bin-mingw64.zip'
+$checksum32 = 'e0383d84ca41028f441c793523109a394c8c45d9ebfe87836036f7e9fdfad39e'
+$checksum64 = '75f35acdb36731886c33346468981796d8657849a8ae2e8b1016a1b46b13ea77'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url32" `
--- automatic/vnote.portable/tools/chocolateyInstall.ps1
+++ automatic/vnote.portable/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'vnote.portable'
-$url32      = 'https://github.com/tamlok/vnote/releases/download/v1.18/VNote_win_x86_portable_1.18.zip'
-$url64      = 'https://github.com/tamlok/vnote/releases/download/v1.18/VNote_win_X64_portable_1.18.zip'
-$checksum32 = '56c887375fff3c290b512cbcb21d7ca99cccea74c21c4ae2befbd37ca731466e'
-$checksum64 = '01f8249d86fa1b0466e4eacd4d64bca63c6264c49a6fee275c50b86f77524adb'
+$url32      = 'https://github.com/tamlok/vnote/releases/download/v1.22/VNote_win_x86_portable_1.22.zip'
+$url64      = 'https://github.com/tamlok/vnote/releases/download/v1.22/VNote_win_X64_portable_1.22.zip'
+$checksum32 = '7c807c29f536a627231ec6ca0334e1da9fc01c799d4d23e6c052bdc21bac3413'
+$checksum64 = 'd6ba3d7294d2861fe2404ba2c34677ee4644f7da37a5e5ddb455919fac331129'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url32" `
