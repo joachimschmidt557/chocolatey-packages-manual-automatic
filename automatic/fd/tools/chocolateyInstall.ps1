29630a50fd472c6c3ef1acbba8ff05582fb1799d
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-09-18 14:17:43 +0200

    Fix fd chocolateyInstall.ps1

--- automatic/fd/tools/chocolateyInstall.ps1
+++ automatic/fd/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $file64      = Get-Item "$toolsDir\*-x86_64-pc-windows-gnu.zip"
 $checksum32 = '864312a830a4a2e29d49aac1bb7c25ab55e223590406e79b8cbc5b16836e58e4'
 $checksum64 = '074e280b3399aa2792cb3ae1fe20afab5f0465d69a07a694dbfbbbea2908211d'
 Install-ChocolateyZipPackage -PackageName "$packageName" `
-                             -Url "$url32" `
-                             -Url64Bit "$url64" `
+                             -File "$file32" `
+                             -File64 "$file64" `
                              -UnzipLocation "$toolsDir" `
                              -Checksum "$checksum32" `
                              -ChecksumType "sha256" `
