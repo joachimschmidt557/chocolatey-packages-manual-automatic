0bf2697637fa0042294162bd75260b17da0bf3ef
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-09-19 10:25:53 +0200

    Correct upx

--- automatic/upx/tools/chocolateyInstall.ps1
+++ automatic/upx/tools/chocolateyInstall.ps1
@@ -2,15 +2,11 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $zipFile    = Get-Item "$toolsDir\upx*.zip"
-$checksum   = '5642d1ae53a6dba7a084d997ca76305e40b897e90b1f7a932e08d980ea1a2ba0'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  file          = $zipFile
-
-  checksum      = $checksum
-  checksumType  = 'sha256'
+  destination   = $toolsDir
+  filefullpath  = $zipFile
 }
 
-Install-ChocolateyZipPackage @packageArgs
+Get-ChocolateyUnzip @packageArgs
