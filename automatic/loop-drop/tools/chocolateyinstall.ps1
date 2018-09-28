e40e5ec230c8ef1e0287232d5e3713be731561a8
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-09-28 20:42:16 +0200

    Fix loop-drop

--- automatic/loop-drop/tools/chocolateyinstall.ps1
+++ automatic/loop-drop/tools/chocolateyinstall.ps1
@@ -1,22 +1,27 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/quodlibet/quodlibet/releases/download/release-4.1.0/quodlibet-4.1.0-installer.exe'
-$checksum   = '34f8fa2892a7a8f52576319cc0db0d228aca2abeee47749faabcfec5be8625eb'
+$url32      = 'https://github.com/mltframework/shotcut/releases/download/v18.09.16/shotcut-win32-180916.exe'
+$url64      = 'https://github.com/mltframework/shotcut/releases/download/v18.09.16/shotcut-win64-180916.exe'
+$checksum32 = 'e14c2f914284d638f4ca2e931e524963ac955da3f4aa14516f9a96611c7671bd'
+$checksum64 = '45750aea65758239eaa0a1b1370e1262edaad278cce412ef6901b6a9165a5fd8'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'MSI'
-  url           = $url
+  fileType      = 'EXE'
+  url           = $url32
+  url64Bit      = $url64
 
-  softwareName  = 'quod libet*'
+  softwareName  = 'loop drop*'
 
-  checksum      = $checksum
+  checksum      = $checksum32
   checksumType  = 'sha256'
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
 
   silentArgs    = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
-  validExitCodes= @(0)
+  validExitCodes= @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
