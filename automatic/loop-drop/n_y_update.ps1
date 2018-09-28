66b0dd8bd581842f5734d771f885315f16d72594
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-09-28 18:40:09 +0200

    Loop Drop almost finished

--- automatic/loop-drop/n_y_update.ps1
+++ automatic/loop-drop/n_y_update.ps1
@@ -17,18 +17,18 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     # Loop.Drop.v3.0.1.x64.msi
-    $re_32  = "Loop.Drop.+.msi"
-    $re_64  = "Loop.Drop.+.x64.msi"
+    $re_32  = "Loop.Drop.v[^A-Za-z]+.msi"
+    $re_64  = "Loop.Drop.v[^A-Za-z]+.x64.msi"
     $url32 = $download_page.links | ? href -match $re_32 | select -First 1 -expand href
     $url64 = $download_page.links | ? href -match $re_64 | select -First 1 -expand href
 
-    #$url32 = "https://github.com" + $url32
-    #$url64 = "https://github.com" + $url64
+    $url32 = "https://github.com" + $url32
+    $url64 = "https://github.com" + $url64
 
-    $version = ($url32 -split '/' | select -last 1 -skip 1) -Replace 'v',''
+    $version = ($url64 -split '/' | select -last 1 -skip 1) -Replace 'v',''
 
     $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
     return $Latest
 }
 
-update -ChecksumFor 32
+update
