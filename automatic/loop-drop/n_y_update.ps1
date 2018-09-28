1b51d7bc466daa33394947dba3130b9702a387ca
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-09-28 18:06:17 +0200

    Add loop-drop

---
+++ automatic/loop-drop/n_y_update.ps1
@@ -0,0 +1,27 @@
+import-module au
+
+$releases = 'https://github.com/quodlibet/quodlibet/releases'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    #quodlibet-4.0.2-installer.exe
+    $re  = "quodlibet-.+-installer.exe"
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+    $version = ($url -split '-' | select -last 1 -Skip 1)
+
+    $Latest = @{ URL = ("https://github.com" + $url); Version = $version }
+    return $Latest
+}
+
+update -ChecksumFor 32
---
+++ automatic/loop-drop/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/quodlibet/quodlibet/releases/download/release-4.1.0/quodlibet-4.1.0-installer.exe'
+$checksum   = '34f8fa2892a7a8f52576319cc0db0d228aca2abeee47749faabcfec5be8625eb'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+
+  softwareName  = 'quod libet*'
+
+  checksum      = $checksum
+  checksumType  = 'sha256'
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
