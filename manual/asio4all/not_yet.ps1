389cf96d0a6f91a6f93877e6fbf09d11579a3f4f
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-09-29 12:31:01 +0200

    Add asio4all

---
+++ manual/asio4all/not_yet.ps1
@@ -0,0 +1,27 @@
+import-module au
+
+$releases = 'https://github.com/IrosTheBeggar/mStream/releases'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    #mstreamExpress-portable-v0.6.zip
+    $re  = "mStreamExpress-installer-v.+.exe"
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+    $version = ($url -split '-' | select -last 1).replace("v",'').replace(".exe","")
+
+    $Latest = @{ URL = ("https://github.com" + $url); Version = $version }
+    return $Latest
+}
+
+update -ChecksumFor 32
---
+++ manual/asio4all/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.asio4all.org/downloads_11/ASIO4ALL_2_14_English.exe'
+$checksum   = '15a3a1ff103f583f9e5493349c85b98799baadee44cb3f25a0611141a4a6d8fa'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'asio4all*'
+
+  checksum      = $checksum
+  checksumType  = 'sha256'
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
