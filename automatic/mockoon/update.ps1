e10aadbfa4b6ab141a2bf9b47ed43972057a3e64
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-10-03 14:12:42 +0200

    Fix (hopefully)

--- automatic/mockoon/update.ps1
+++ automatic/mockoon/update.ps1
@@ -4,6 +4,7 @@ $releases = 'https://github.com/255kb/mockoon/releases'
 
 function global:au_BeforeUpdate() {
     #Download $Latest.URL32 / $Latest.URL64 in tools directory and remove any older installers.
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
     Get-RemoteFiles -Purge
 }
 
@@ -44,4 +45,4 @@ function global:au_GetLatest {
     #}
 }
 
-update -ChecksumFor 32
+update -ChecksumFor none
