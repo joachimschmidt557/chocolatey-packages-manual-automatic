f7da3d24c82a42ee45853f4e3fe054c462c2b78e
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-10-03 19:00:22 +0200

    Enable hyper

--- automatic/hyper/update.ps1
+++ automatic/hyper/update.ps1
@@ -5,6 +5,7 @@ $releases = 'https://github.com/zeit/hyper/releases'
 function global:au_BeforeUpdate() {
     #Download $Latest.URL32 / $Latest.URL64 in tools directory and remove any older installers.
     Get-RemoteFiles -Purge
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
 }
 
 function global:au_SearchReplace {
@@ -44,4 +45,4 @@ function global:au_GetLatest {
     }
 }
 
-update -ChecksumFor 32
+update -ChecksumFor none
