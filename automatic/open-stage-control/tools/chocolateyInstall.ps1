7cd5c4dcb272f2cccf6fb3ca51c4a5f41537101d
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-10-04 14:10:26 +0200

    Add open-stage-control

---
+++ automatic/open-stage-control/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$zipFile    = Get-Item "$toolsDir\*-i686-pc-windows-gnu*.zip"
+$zipFile64  = Get-Item "$toolsDir\*-x86_64-pc-windows-gnu*.zip"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = $toolsDir
+  filefullpath  = $zipFile
+  filefullpath64= $zipFile64
+}
+
+Get-ChocolateyUnzip @packageArgs
