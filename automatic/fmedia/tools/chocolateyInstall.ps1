644edaff9f79d5e8dbdd9794bb0b03124ce92863
Chocolatey <chocolatey@realdimensions.net>
2018-10-14 12:32:15 +0000

    AU: 2 updated - fmedia open-stage-control [skip ci] https://gist.github.com/8a2f24b529db5159a7c189c3b4ccd9a4/4541886d85b3109caafbbda6e26a6adc22f660ef

--- automatic/fmedia/tools/chocolateyInstall.ps1
+++ automatic/fmedia/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'fmedia'
-$url32      = 'http://fmedia.firmdev.com/fmedia-1.0.1-win-x64.zip'
-$url64      = 'http://fmedia.firmdev.com/fmedia-1.0.1-win-x86.zip'
-$checksum32 = '69aa712447f191f1d476fb3eea0a5d0153237bc6aeec891321bdc3032c9d8952'
-$checksum64 = '273234a43ad11b85e6929a969c122851978bc942066c6adc05ee9b46b6b723ff'
+$url32      = 'http://fmedia.firmdev.com/fmedia-1.1-win-x64.zip'
+$url64      = 'http://fmedia.firmdev.com/fmedia-1.1-win-x86.zip'
+$checksum32 = '05522d715dac36051a21ea78b076e495c9e3ab9d774b581c618f8ebc0133a662'
+$checksum64 = '48ae42052bc256acbd679ae8b6bccf58f28c897b195e06be0a3486ca0e8eed3a'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url32" `
