49234b13ea8280e7fa9eb5aff37f40947738a2c4
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-11-10 19:08:39 +0100

    Fix quodlibet

--- automatic/quodlibet.portable/tools/chocolateyinstall.ps1
+++ automatic/quodlibet.portable/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/quodlibet/quodlibet/releases/download/release-4.1.0/quodlibet-4.1.0-portable.exe'
+$url        = 'https://github.com/quodlibet/quodlibet/releases/download/release-4.2.0/quodlibet-4.2.0-portable.exe'
 $checksum   = '13C6007BB20329CACE4596651D09983F8C8CE5D37B2CAA4DC2837CAB4D088988'
 
 Get-ChocolateyWebFile -PackageName 'quodlibet.portable' -FileFullPath "$toolsDir\QuodLibet.exe" -Url $url -Checksum $checksum -ChecksumType 'sha256'
