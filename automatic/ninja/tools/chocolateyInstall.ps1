2556bc11ff94436b425a181198240cb3a8fe0bd2
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-11-19 18:17:54 +0100

    WIP ninja

--- automatic/ninja/tools/chocolateyInstall.ps1
+++ automatic/ninja/tools/chocolateyInstall.ps1
@@ -1,14 +1,14 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$zipFile    = Get-Item "$toolsDir\*-i686-pc-windows-gnu*.zip"
-$zipFile64  = Get-Item "$toolsDir\*-x86_64-pc-windows-gnu*.zip"
+$zipFile    = Get-Item "$toolsDir\ninja-win.zip"
+#$zipFile64  = Get-Item "$toolsDir\*-x86_64-pc-windows-gnu*.zip"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = $toolsDir
   filefullpath  = $zipFile
-  filefullpath64= $zipFile64
+  #filefullpath64= $zipFile64
 }
 
 Get-ChocolateyUnzip @packageArgs
