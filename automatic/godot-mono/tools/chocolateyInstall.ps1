fe8159dd65cc938e745a8dab54d63bcbe11a410c
joachimschmidt557 <joachim.schmidt557@outlook.com>
2018-12-19 09:58:04 +0100

    Finish godot-mono

---
+++ automatic/godot-mono/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$zipFile    = Get-Item "$toolsDir\*stable_win32*.zip"
+$zipFile64  = Get-Item "$toolsDir\*stable_win64*.zip"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = $toolsDir
+  filefullpath  = $zipFile
+  filefullpath64= $zipFile64
+}
+
+Get-ChocolateyUnzip @packageArgs
