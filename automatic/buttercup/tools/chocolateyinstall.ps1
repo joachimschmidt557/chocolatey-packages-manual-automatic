0cecf587b5a6ead52b80c7779d2652d9b28f893d
Chocolatey <chocolatey@realdimensions.net>
2018-12-20 06:01:26 +0000

    AU: 1 updated - buttercup [skip ci] https://gist.github.com/8a2f24b529db5159a7c189c3b4ccd9a4/44ac55eba2e9ceed0e3d6439f905a921a8e49284

--- automatic/buttercup/tools/chocolateyinstall.ps1
+++ automatic/buttercup/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/buttercup/buttercup-desktop/releases/download/v1.12.0/buttercup-desktop-setup-1.12.0.exe'
-$checksum   = '1a2945c7211b8b5e866d03d5f90b2c6602472fdd35fe381801949c06bf2dc1cd'
+$url        = 'https://github.com/buttercup/buttercup-desktop/releases/download/v1.13.0/buttercup-desktop-setup-1.13.0.exe'
+$checksum   = 'ad74982372f6da48abbdb5bd4afa8ad2b3446c221820649fe5d3a8fbeb662b96'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
