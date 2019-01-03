d2c28127b0574121b6c6be76b4ffbdfa3811bee8
Chocolatey <chocolatey@realdimensions.net>
2019-01-03 06:02:19 +0000

    AU: 2 updated - buttercup open-stage-control [skip ci] https://gist.github.com/8a2f24b529db5159a7c189c3b4ccd9a4/35db44ab451b809504a2bc0be1e3316ff251d9a8

--- automatic/buttercup/tools/chocolateyinstall.ps1
+++ automatic/buttercup/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/buttercup/buttercup-desktop/releases/download/v1.13.0/buttercup-desktop-setup-1.13.0.exe'
-$checksum   = 'ad74982372f6da48abbdb5bd4afa8ad2b3446c221820649fe5d3a8fbeb662b96'
+$url        = 'https://github.com/buttercup/buttercup-desktop/releases/download/v1.14.0/buttercup-desktop-setup-1.14.0.exe'
+$checksum   = 'a0f0de28e85bdc089dc0fc57597dab2b6a9337cec6bc68a276ef171900ea5155'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
