988ff3e85b853eeeb89273a19eecd362752054c7
Chocolatey <chocolatey@realdimensions.net>
2019-01-09 06:02:21 +0000

    AU: 2 updated - hyper sharpkeys.portable [skip ci] https://gist.github.com/8a2f24b529db5159a7c189c3b4ccd9a4/ae2956a6057dd52d247bfbc4c2c30bf4471204a9

--- automatic/sharpkeys.portable/tools/chocolateyInstall.ps1
+++ automatic/sharpkeys.portable/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'sharpkeys.portable'
-$url = 'https://github.com/randyrants/sharpkeys/releases/download/v3.8/sharpkeys38.zip'
-$checksum = '3d7b22ff9a3d7abf90e8ef81c72bc328e561682bed1a03b1b574b88333c35956'
+$url = 'https://github.com/randyrants/sharpkeys/releases/download/v3.9/sharpkeys39.zip'
+$checksum = 'fb71358170655549c5eb25dc3c566eeaecf1d5e35e326b0b3f15b29402a010c6'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
