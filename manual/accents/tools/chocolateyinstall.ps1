47c286c94ccea8db1451fa0c4a8f20ff22935e51
joachimschmidt557 <joachim.schmidt557@outlook.com>
2019-01-26 16:59:58 +0100

    Maybe fix accents?

--- manual/accents/tools/chocolateyinstall.ps1
+++ manual/accents/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 Install-ChocolateyZipPackage -PackageName 'Accents' `
-  -Url 'https://github.com/IrosTheBeggar/mStream/releases/download/v3.2.0/mstreamExpress-portable-v0.6.zip' `
+  -Url 'http://www.dcmembers.com/skrommel/download/accents/?wpdmdl=6' `
   -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)" `
   -Checksum "D6B06BA79E4E67BC5A09E64EACE99E9A91E92F2F691CA7852D761BE26B1FEABF" `
   -ChecksumType "sha256"
