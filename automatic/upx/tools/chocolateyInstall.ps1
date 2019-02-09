fbafc4ee3b18d0b2bd523bc72791b6d0bd87cab0
joachimschmidt557 <joachim.schmidt557@outlook.com>
2019-02-09 19:14:11 +0100

    Update upx

--- automatic/upx/tools/chocolateyInstall.ps1
+++ automatic/upx/tools/chocolateyInstall.ps1
@@ -1,12 +1,14 @@
 
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$zipFile    = Get-Item "$toolsDir\upx*.zip"
+$zipFile    = Get-Item "$toolsDir\*win32*.zip"
+$zipFile64  = Get-Item "$toolsDir\*win64*.zip"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = $toolsDir
   filefullpath  = $zipFile
+  filefullpath64= $zipFile64
 }
 
 Get-ChocolateyUnzip @packageArgs
--- automatic/upx/update.ps1
+++ automatic/upx/update.ps1
@@ -5,13 +5,17 @@ $releases = 'https://github.com/upx/upx/releases'
 function global:au_BeforeUpdate() {
     #Download $Latest.URL32 / $Latest.URL64 in tools directory and remove any older installers.
     Get-RemoteFiles -Purge
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.URL64
 }
 
 function global:au_SearchReplace {
     @{
         ".\legal\VERIFICATION.txt" = @{
-            "(?i)(\s+URL:).*"             = "`${1} $($Latest.URL32)"
-            "(?i)(checksum:).*"           = "`${1} $($Latest.Checksum32)"
+            "(?i)(\s+32-bit:).*"             = "`${1} $($Latest.URL32)"
+            "(?i)(\s+64-bit:).*"             = "`${1} $($Latest.URL64)"
+            "(?i)(checksum32:).*"           = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"           = "`${1} $($Latest.Checksum64)"
           }
      }
 }
@@ -20,14 +24,18 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     #upx394w.zip
-    $re  = "upx.+w.zip"
-    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
-    $url = "https://github.com" + $url
+    $re_32  = "upx-.+win32.zip"
+    $re_64  = "upx-.+win64.zip"
+    $url32  = $download_page.links | ? href -match $re_32 | select -First 1 -expand href
+    $url64  = $download_page.links | ? href -match $re_64 | select -First 1 -expand href
 
-    $version = ($url -split '/' | select -last 1 -skip 1) -Replace 'v',''
+    $url32  = "https://github.com" + $url32
+    $url64  = "https://github.com" + $url64
 
-    $Latest = @{ URL = $url; Version = $version }
+    $version = ($url32 -split '/' | select -last 1 -skip 1) -Replace 'v',''
+
+    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
     return $Latest
 }
 
-update -ChecksumFor 32
+update -ChecksumFor none
