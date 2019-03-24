fbec4c83b9b5d13c37fe80d3348025d60ab60bc7
joachimschmidt557 <joachim.schmidt557@outlook.com>
2019-03-24 14:27:19 +0100

    Why is this not working?

--- automatic/mstream.portable/update.ps1
+++ automatic/mstream.portable/update.ps1
@@ -15,7 +15,7 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     #mstreamExpress-Windows-x64-portable.zip
-    $re  = "mStreamExpress-Windows-x64-portable.zip"
+    $re  = "mStreamExpress.*-Windows-x64-portable.zip"
     $url = $download_page.links | ? href -match $re | select -First 1 -expand href
 
     $version = ($url32 -split '/' | select -last 1 -skip 1) -Replace 'v',''
