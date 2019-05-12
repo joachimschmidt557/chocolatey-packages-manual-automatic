54504f93147d869f40dac6e452126ca30bbda242
Chocolatey <chocolatey@realdimensions.net>
2019-05-12 06:02:15 +0000

    AU: 1 updated - vnote.portable [skip ci] https://gist.github.com/8a2f24b529db5159a7c189c3b4ccd9a4/78995cb7cedf56fabd9226604a01aa82f84e4718

--- automatic/vnote.portable/tools/chocolateyInstall.ps1
+++ automatic/vnote.portable/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'vnote.portable'
-$url32      = 'https://github.com/tamlok/vnote/releases/download/2.4/VNote_win_x86_portable_2.4.zip'
-$url64      = 'https://github.com/tamlok/vnote/releases/download/2.4/VNote_win_X64_portable_2.4.zip'
-$checksum32 = 'b258fb08c81a8c9fc11b47c6163fd15d408af8a1bfa21eb8fbeab8297227fa56'
-$checksum64 = 'c8982d826e6656ea7a718d06aa9a6ba3d7d1a0d0d952d32d28ff7cba504aaeaa'
+$url32      = 'https://github.com/tamlok/vnote/releases/download/v2.5/VNote_win_x86_portable_2.5.zip'
+$url64      = 'https://github.com/tamlok/vnote/releases/download/v2.5/VNote_win_X64_portable_2.5.zip'
+$checksum32 = '881b22f7368f7e0824a47df3690665b0e1a8d12d205d5c56bd2734c21e0bb66c'
+$checksum64 = 'cbb78cec615afcbecc1914f7a1661db56d14b585603b30b370d215a2cd0fe49e'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url32" `
