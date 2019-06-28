a5c6e3dae68074420fe04b9c27a64e62594b7e71
joachimschmidt557 <joachim.schmidt557@outlook.com>
2019-06-28 09:30:34 +0200

    WIP jami

--- automatic/jami/update.ps1
+++ automatic/jami/update.ps1
@@ -6,8 +6,6 @@ function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
             "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
             "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
      }
@@ -32,4 +30,4 @@ function global:au_GetLatest {
     return $Latest
 }
 
-update -ChecksumFor 32
+update -ChecksumFor 64
