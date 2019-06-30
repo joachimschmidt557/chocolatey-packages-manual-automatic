2015af8f2efe2d7790447105f68f9d1f2fb57c47
Chocolatey <chocolatey@realdimensions.net>
2019-06-30 06:01:20 +0000

    AU: 1 updated - jami [skip ci] https://gist.github.com/8a2f24b529db5159a7c189c3b4ccd9a4/d1845713e4645317f5583035fc21449032b20c70

--- automatic/jami/tools/chocolateyinstall.ps1
+++ automatic/jami/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://dl.ring.cx/windows/ring-windows-nightly_x86_64-20190607420583~158.exe'
-$checksum64 = 'ad08ff17544e76963b426bb91ebce9d68f155a41be3dba475f0bef5426369f53'
+$url64      = 'https://dl.ring.cx/windows/ring-windows-nightly_x86_64-20190629122649~160.exe'
+$checksum64 = 'aabfc4bd00d972354f4730b33dfb7d5675582751185bd4f4ca62f0f7a95c0af9'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
