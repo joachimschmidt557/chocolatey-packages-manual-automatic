efab0d411a8336702e0338978e5c1b400a608a8b
Chocolatey <chocolatey@realdimensions.net>
2020-01-17 17:52:39 +0000

    AU: 1 updated - shotcut.portable [skip ci] https://gist.github.com/8a2f24b529db5159a7c189c3b4ccd9a4/0660bef5ac29ac3bc7bbc95ff8a843f6c731d447

--- automatic/shotcut.portable/tools/chocolateyInstall.ps1
+++ automatic/shotcut.portable/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'shotcut.portable'
-$url32      = 'https://github.com/mltframework/shotcut/releases/download/v19.10.20/shotcut-win32-191020.zip'
-$url64      = 'https://github.com/mltframework/shotcut/releases/download/v19.10.20/shotcut-win64-191020.zip'
-$checksum32 = 'ac7a6c8df06e9f24310b56c8c9dd373450bb9c05d261781615c0b0d87abfc41d'
-$checksum64 = 'f6a56c8ef1f115325b21abad3ab0a78351b4931031595eeb67d1b71433edf985'
+$url32      = 'https://github.com/mltframework/shotcut/releases/download/v19.12.31/shotcut-win32-191231.zip'
+$url64      = 'https://github.com/mltframework/shotcut/releases/download/v19.12.31/shotcut-win64-191231.zip'
+$checksum32 = '55257bf23aba1687c47d104dda10ebb799e0f068027277f7f7ef2399c8b5f316'
+$checksum64 = 'ccdff18537d46cdd177d5df82d7b5cf8bbb6cdbcf390e44fec84717fbc997b70'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url32" `
