37b0ff1809b08965e4459f8243382d64779a346e
joachimschmidt557 <joachim.schmidt557@outlook.com>
2021-02-01 13:21:24 +0100

    Shotcut packages only available for x86_64

--- automatic/shotcut.portable/tools/chocolateyInstall.ps1
+++ automatic/shotcut.portable/tools/chocolateyInstall.ps1
@@ -1,14 +1,9 @@
 ﻿$packageName = 'shotcut.portable'
-$url32      = 'https://github.com/mltframework/shotcut/releases/download/v20.09.27/shotcut-win32-200927.zip'
 $url64      = 'https://github.com/mltframework/shotcut/releases/download/v20.09.27/shotcut-win64-200927.zip'
-$checksum32 = '7a4422c47e01ec899a5d3b8cd4230546ba549c73d626977dee5f82adac678c32'
 $checksum64 = '31caf6ec3644877d12aeadd60e3dea65cf3fad29dc6a2bb2e33e7c8fbad6e711'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
-                             -Url "$url32" `
                              -Url64Bit "$url64" `
                              -UnzipLocation "$toolsDir" `
-                             -Checksum "$checksum32" `
-                             -ChecksumType "sha256" `
                              -Checksum64 "$checksum64" `
                              -ChecksumType64 "sha256" `
--- automatic/shotcut.portable/update.ps1
+++ automatic/shotcut.portable/update.ps1
@@ -1,13 +1,11 @@
 import-module au
 
-$releases = 'https://www.shotcut.org/download/'
+$releases = 'https://github.com/mltframework/shotcut/releases'
 
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
             "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
             "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
      }
@@ -17,17 +15,14 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
 
     # https://github.com/mltframework/shotcut/releases/download/v18.03/shotcut-win64-180306.zip
-    $re_32  = "shotcut-win32-.+.zip"
     $re_64  = "shotcut-win64-.+.zip"
-    $url32 = $download_page.links | ? href -match $re_32 | select -First 1 -Skip 1 -expand href
     $url64 = $download_page.links | ? href -match $re_64 | select -First 1 -Skip 1 -expand href
 
-    #$url32 = "https://github.com" + $url32
-    #$url64 = "https://github.com" + $url64
+    $url64 = "https://github.com" + $url64
 
-    $version = ($url32 -split '/' | select -last 1 -skip 1) -Replace 'v',''
+    $version = ($url64 -split '/' | select -last 1 -skip 1) -Replace 'v',''
 
-    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+    $Latest = @{ URL64 = $url64; Version = $version }
     return $Latest
 }
 
