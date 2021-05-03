e3c9afb23742ae5a2dbd96f502e608f080d32fa2
Chocolatey <chocolatey@realdimensions.net>
2021-05-03 06:01:42 +0000

    AU: 2 updated - Shotcut shotcut.install [skip ci] https://gist.github.com/8a2f24b529db5159a7c189c3b4ccd9a4/1a9752c697e4e22fe366fb2a961293b8a7e399f8

--- automatic/shotcut.install/tools/chocolateyInstall.ps1
+++ automatic/shotcut.install/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/mltframework/shotcut/releases/download/v21.04.13/shotcut-win64-210413.exe'
-$checksum64 = 'bd80c11ede100d415969ec8a73205455bf77b3d2bd62509155bdc143bad9211c'
+$url64      = 'https://github.com/mltframework/shotcut/releases/download/v21.05.01/shotcut-win64-210501.exe'
+$checksum64 = '0ebe4a5826ac24e8edfdf012684b8d8b080b2ccb40564e4619e2c2a5fa8b1f0f'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
