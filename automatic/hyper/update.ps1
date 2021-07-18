37bacdd61bf596234ed36adb888ff0658d749677
joachimschmidt557 <joachim.schmidt557@outlook.com>
2021-07-18 17:50:38 +0200

    update hyper updater

--- automatic/hyper/update.ps1
+++ automatic/hyper/update.ps1
@@ -5,14 +5,14 @@ $releases = 'https://github.com/zeit/hyper/releases'
 function global:au_BeforeUpdate() {
     #Download $Latest.URL32 / $Latest.URL64 in tools directory and remove any older installers.
     Get-RemoteFiles -Purge
-    $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.URL64
 }
 
 function global:au_SearchReplace {
     @{
         ".\legal\VERIFICATION.txt" = @{
-            "(?i)(\s+URL:).*"             = "`${1} $($Latest.URL32)"
-            "(?i)(checksum:).*"           = "`${1} $($Latest.Checksum32)"
+            "(?i)(\s+URL:).*"             = "`${1} $($Latest.URL64)"
+            "(?i)(checksum:).*"           = "`${1} $($Latest.Checksum64)"
           }
      }
 }
@@ -20,19 +20,19 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    # Release: hyper-Setup-2.0.0.exe
-    $re_release     = "hyper-Setup-[^A-Za-z]+.exe"
+    # Release: Hyper-Setup-2.0.0.exe
+    $re_release     = "Hyper-Setup-[^A-Za-z]+.exe"
     $url_release    = $download_page.links | ? href -match $re_release | select -First 1 -expand href
     $url_release    = "https://github.com" + $url_release
 
-    $version_release= ($url_release -split '/' | select -last 1 -skip 1)
+    $version_release= ($url_release -split '/' | select -last 1 -skip 1) -Replace 'v',''
 
-    # Canary: hyper-Setup-2.1.0-canary.2.exe
-    $re_canary      = "hyper-Setup-[^A-Za-z]+-canary.[^A-Za-z]+.exe"
+    # Canary: Hyper-Setup-2.1.0-canary.2.exe
+    $re_canary      = "Hyper-Setup-[^A-Za-z]+-canary.[^A-Za-z]+.exe"
     $url_canary     = $download_page.links | ? href -match $re_canary | select -First 1 -expand href
     $url_canary     = "https://github.com" + $url_canary
 
-    $version_canary = ($url_canary -split '/' | select -last 1 -skip 1)
+    $version_canary = ($url_canary -split '/' | select -last 1 -skip 1) -Replace 'v',''
     $version_canary = ($version_canary.split(".") | select -first 3) -join '.'
 
     #$Latest = @{ URL = $url; Version = $version }
@@ -40,8 +40,8 @@ function global:au_GetLatest {
 
     @{
         Streams = [ordered] @{
-            'release' = @{ Version = $version_release; URL32 = $url_release }
-            'canary' = @{ Version = $version_canary; URL32 = $url_canary }
+            'release' = @{ Version = $version_release; URL64 = $url_release }
+            'canary' = @{ Version = $version_canary; URL64 = $url_canary }
         }
     }
 }
