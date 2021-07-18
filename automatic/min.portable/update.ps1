da34e9918a074b4bcee91107200af57ba11ff1eb
joachimschmidt557 <joachim.schmidt557@outlook.com>
2021-07-18 17:38:10 +0200

    min x86_64 only

--- automatic/min.portable/update.ps1
+++ automatic/min.portable/update.ps1
@@ -6,8 +6,8 @@ function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
             "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            # "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            # "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
             "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
      }
@@ -18,18 +18,18 @@ function global:au_GetLatest {
 
     # Min-v1.7.0-win32-ia32.zip
     # Min-v1.7.0-win32-x64.zip
-    $re_32  = "Min-v[^A-Za-z]+-win32-ia32.zip"
+    # $re_32  = "Min-v[^A-Za-z]+-win32-ia32.zip"
     $re_64  = "Min-v[^A-Za-z]+-win32-x64.zip"
-    $url32 = $download_page.links | ? href -match $re_32 | select -First 1 -expand href
+    # $url32 = $download_page.links | ? href -match $re_32 | select -First 1 -expand href
     $url64 = $download_page.links | ? href -match $re_64 | select -First 1 -expand href
 
-    $url32 = "https://github.com" + $url32
+    # $url32 = "https://github.com" + $url32
     $url64 = "https://github.com" + $url64
 
-    $version = ($url32 -split '-' | select -last 1 -skip 2) -Replace 'v',''
+    $version = ($url64 -split '-' | select -last 1 -skip 2) -Replace 'v',''
 
-    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+    $Latest = @{ URL64 = $url64; Version = $version }
     return $Latest
 }
 
-update
+update -ChecksumFor 64
