a6c8892af84cb78bc96e531dfeedbfe46b556faa
joachimschmidt557 <joachim.schmidt557@outlook.com>
2021-07-18 18:13:26 +0200

    update buttercup chocolateyInstall.ps1

--- automatic/buttercup/tools/chocolateyinstall.ps1
+++ automatic/buttercup/tools/chocolateyinstall.ps1
@@ -1,19 +1,19 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/buttercup/buttercup-desktop/releases/download/v1.20.5/Buttercup-Setup-1.20.5.exe'
-$checksum   = 'c371e9662dafb66d75ac9e6d06be9a8309fe21a335544d1bd90af4b1a7b7e39e'
+$url64      = 'https://github.com/buttercup/buttercup-desktop/releases/download/v1.20.5/Buttercup-Setup-1.20.5.exe'
+$checksum64 = 'c371e9662dafb66d75ac9e6d06be9a8309fe21a335544d1bd90af4b1a7b7e39e'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url
+  url64Bit      = $url64
 
   softwareName  = 'buttercup*'
 
-  checksum      = $checksum
-  checksumType  = 'sha256'
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
 
   silentArgs   = '/S'
   validExitCodes= @(0)
