bd53369d3f4dff44ff10e409b94ef1df59fb2db8
Chocolatey <chocolatey@realdimensions.net>
2021-10-02 06:02:55 +0000

    AU: 4 updated - godot godot-mono mstream mstream.install [skip ci] https://gist.github.com/8a2f24b529db5159a7c189c3b4ccd9a4/cdec69c891813f6eba0da0c80d072b18859b49de

--- automatic/mstream.install/tools/chocolateyinstall.ps1
+++ automatic/mstream.install/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/IrosTheBeggar/mStream/releases/download/v5.5.0/mStream-Server-Setup-5.5.0.exe'
-$checksum   = '64ba1132de5eeaad27782d0432cec21e7218c5587669c895922eeafb42ceb5f7'
+$url        = 'https://github.com/IrosTheBeggar/mStream/releases/download/v5.6.1/mStream-Server-Setup-5.6.1.exe'
+$checksum   = '000643a379ad6dc0fa4385582221d24fdd1f4012e3d6eb13464bb899134023c0'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
