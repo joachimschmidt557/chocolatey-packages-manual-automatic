9b20cbb388451a30ef48fe0ccdb8f9cb8aa46269
Chocolatey <chocolatey@realdimensions.net>
2021-11-15 06:01:15 +0000

    AU: 1 updated - min.portable [skip ci] https://gist.github.com/8a2f24b529db5159a7c189c3b4ccd9a4/30c4b62dda8d09d8f90e35c63fe21ae5754cc81b

--- automatic/min.portable/tools/chocolateyInstall.ps1
+++ automatic/min.portable/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'min.portable'
 $url32      = 'https://github.com/minbrowser/min/releases/download/v1.19.2/Min-v1.19.2-win32-ia32.zip'
-$url64      = 'https://github.com/minbrowser/min/releases/download/v1.22.0/Min-v1.22.0-win32-x64.zip'
+$url64      = 'https://github.com/minbrowser/min/releases/download/v1.22.1/Min-v1.22.1-win32-x64.zip'
 $checksum32 = 'b924f3d3f31cc502fa82022b75632e1009d317a8c5eaf5ec01d3518571f07b81'
-$checksum64 = '507adb9a37fc37de82b059e11e4a818aa4248b113ccf5bbf3beffc0fbc95aee3'
+$checksum64 = 'cc4292394af13df35e0de6ef88cb5822859bde56dafd2867c477469ecd594c4e'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url32" `
