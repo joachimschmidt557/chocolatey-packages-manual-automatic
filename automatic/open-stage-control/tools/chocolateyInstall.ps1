d22798832aa5259bd16ca98ea511e4fde8db0795
joachimschmidt557 <joachim.schmidt557@outlook.com>
2021-12-18 20:07:50 +0100

    open-stage-control: remove 32-bit version from chocolateyInstall.ps1

--- automatic/open-stage-control/tools/chocolateyInstall.ps1
+++ automatic/open-stage-control/tools/chocolateyInstall.ps1
@@ -1,13 +1,11 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$zipFile    = Get-Item "$toolsDir\open-stage-control*ia32*.zip"
 $zipFile64  = Get-Item "$toolsDir\open-stage-control*x64*.zip"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = $toolsDir
-  filefullpath  = $zipFile
   filefullpath64= $zipFile64
 }
 
