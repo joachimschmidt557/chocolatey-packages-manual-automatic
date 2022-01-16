23887c0f6b4691397f27ce8fd68a54f6c616bcdf
joachimschmidt557 <joachim.schmidt557@outlook.com>
2022-01-16 17:02:03 +0100

    Add yt-dlg.portable

---
+++ automatic/yt-dlg.portable/update.ps1
@@ -0,0 +1,37 @@
+import-module au
+
+$releases = 'https://github.com/oleksis/youtube-dl-gui/releases'
+
+function global:au_BeforeUpdate() {
+    #Download $Latest.URL32 / $Latest.URL64 in tools directory and remove any older installers.
+    Get-RemoteFiles -Purge
+}
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            #"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+URL:).*"             = "`${1} $($Latest.URL32)"
+            "(?i)(checksum:).*"           = "`${1} $($Latest.Checksum32)"
+          }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    # yt-dlg.exe
+    $re  = "yt-dlg.exe"
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+    $version = ($url -split '/' | select -last 1 -Skip 1) -Replace 'v',''
+
+    $Latest = @{ URL = ("https://github.com" + $url); Version = $version }
+    return $Latest
+}
+
+update -ChecksumFor 32
