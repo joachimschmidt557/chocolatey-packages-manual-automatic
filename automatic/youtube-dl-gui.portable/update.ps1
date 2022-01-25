efc1380a57fe4f84cba8008dff1ba6ff72f9a417
Smart123s <28480228+Smart123s@users.noreply.github.com>
2022-01-25 17:30:08 +0100

    Update yt-dlg.portable
    Replace the abandoned repositroy with a continuation
    Update update.ps1 to the new repo
    Add oleksis as author in nuspec
    Update links in nuspec
    Update package to latest (v1.8.3) version

--- automatic/youtube-dl-gui.portable/update.ps1
+++ automatic/youtube-dl-gui.portable/update.ps1
@@ -1,19 +1,14 @@
 import-module au
 
-$releases = 'https://github.com/MrS0m30n3/youtube-dl-gui/releases'
+$releases = 'https://github.com/oleksis/youtube-dl-gui/releases'
 
 function global:au_BeforeUpdate() {
     #Download $Latest.URL32 / $Latest.URL64 in tools directory and remove any older installers.
-    Get-RemoteFiles -Purge
+    Get-RemoteFiles -Purge -NoSuffix
 }
 
 function global:au_SearchReplace {
     @{
-        'tools\chocolateyInstall.ps1' = @{
-            #"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
-            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-        }
-
         ".\legal\VERIFICATION.txt" = @{
             "(?i)(\s+URL:).*"             = "`${1} $($Latest.URL32)"
             "(?i)(checksum:).*"           = "`${1} $($Latest.Checksum32)"
@@ -24,14 +19,13 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    # youtube-dl-gui-0.4-win-portable.zip
-    $re  = "youtube-dl-gui-.+-win-portable.zip"
+    $re  = "yt-dlg.exe"
     $url = $download_page.links | ? href -match $re | select -First 1 -expand href
 
-    $version = ($url -split '-' | select -last 1 -Skip 2)
+    $version = ( $url -split '/' | select -last 1 -Skip 1 | % SubString(1) )
 
-    $Latest = @{ URL = ("https://github.com" + $url); Version = $version }
+    $Latest = @{ URL32 = ("https://github.com" + $url); Version = $version }
     return $Latest
 }
 
-update -ChecksumFor 32
+Update-Package -ChecksumFor none
