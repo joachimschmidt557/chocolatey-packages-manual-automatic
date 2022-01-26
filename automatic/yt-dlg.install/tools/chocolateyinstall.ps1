287a0c750f796ecdbb8fcf561ea69597d2fd56bd
joachimschmidt557 <joachim.schmidt557@outlook.com>
2022-01-26 21:23:38 +0100

    Add yt-dlg.install

---
+++ automatic/yt-dlg.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Get-Item "$toolsDir\yt-dlg*.msi"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'MSI'
+  file          = $fileLocation
+
+  silentArgs    = "/qn /norestart"
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'yt-dlg*'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/yt-dlg.install/update.ps1
@@ -0,0 +1,32 @@
+import-module au
+
+$releases = 'https://github.com/oleksis/youtube-dl-gui/releases'
+
+function global:au_BeforeUpdate() {
+    #Download $Latest.URL32 / $Latest.URL64 in tools directory and remove any older installers.
+    Get-RemoteFiles -Purge
+}
+
+function global:au_SearchReplace {
+    @{
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+URL:).*"             = "`${1} $($Latest.URL32)"
+            "(?i)(checksum:).*"           = "`${1} $($Latest.Checksum32)"
+          }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    # yt-dlg*.msi
+    $re  = "yt-dlg*.msi"
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+    $version = ($url -split '/' | select -last 1 -Skip 1) -Replace 'v',''
+
+    $Latest = @{ URL32 = ("https://github.com" + $url); Version = $version }
+    return $Latest
+}
+
+update -ChecksumFor 32
