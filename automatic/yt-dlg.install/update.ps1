f9977875e779a8b84abefd157e68b50d38794ea0
joachimschmidt557 <joachim.schmidt557@outlook.com>
2022-03-08 18:28:18 +0100

    Try to fix yt-dlg.install

--- automatic/yt-dlg.install/update.ps1
+++ automatic/yt-dlg.install/update.ps1
@@ -5,13 +5,14 @@ $releases = 'https://github.com/oleksis/youtube-dl-gui/releases'
 function global:au_BeforeUpdate() {
     #Download $Latest.URL32 / $Latest.URL64 in tools directory and remove any older installers.
     Get-RemoteFiles -Purge
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.URL64
 }
 
 function global:au_SearchReplace {
     @{
         ".\legal\VERIFICATION.txt" = @{
-            "(?i)(\s+URL:).*"             = "`${1} $($Latest.URL32)"
-            "(?i)(checksum:).*"           = "`${1} $($Latest.Checksum32)"
+            "(?i)(\s+URL:).*"             = "`${1} $($Latest.URL64)"
+            "(?i)(checksum:).*"           = "`${1} $($Latest.Checksum64)"
           }
     }
 }
@@ -19,14 +20,13 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    # yt-dlg.*.msi
     $re  = "yt-dlg.*.msi"
     $url = $download_page.links | ? href -match $re | select -First 1 -expand href
 
     $version = ($url -split '/' | select -last 1 -Skip 1) -Replace 'v',''
 
-    $Latest = @{ URL32 = ("https://github.com" + $url); Version = $version }
+    $Latest = @{ URL64 = ("https://github.com" + $url); Version = $version }
     return $Latest
 }
 
-update -ChecksumFor 32
+update -ChecksumFor none
