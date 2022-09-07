aeed6ec5bb8ba9035127890ebbf3f2e736150685
joachimschmidt557 <joachim.schmidt557@outlook.com>
2022-09-07 12:31:43 +0200

    update asio4all checksum

--- manual/asio4all/tools/chocolateyinstall.ps1
+++ manual/asio4all/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'http://www.asio4all.org/downloads_11/ASIO4ALL_2_15_English.exe'
-$checksum   = '15a3a1ff103f583f9e5493349c85b98799baadee44cb3f25a0611141a4a6d8fa'
+$checksum   = '74dfe576c6f16e362ff2d44c313947aad9d63ed839b4d3ffac88a1320a486b90'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
