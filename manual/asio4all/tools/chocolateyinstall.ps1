8d51098fad99c78e50507b2e816cf3fa1579ecba
joachimschmidt557 <joachim.schmidt557@outlook.com>
2022-09-07 11:22:20 +0200

    Update asio4all to 2.15

--- manual/asio4all/tools/chocolateyinstall.ps1
+++ manual/asio4all/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://www.asio4all.org/downloads_11/ASIO4ALL_2_14_English.exe'
+$url        = 'http://www.asio4all.org/downloads_11/ASIO4ALL_2_15_English.exe'
 $checksum   = '15a3a1ff103f583f9e5493349c85b98799baadee44cb3f25a0611141a4a6d8fa'
 
 $packageArgs = @{
