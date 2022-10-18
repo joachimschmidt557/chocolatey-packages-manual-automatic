6d1cc36b8605c27b87cd064703c70c95eb79ec59
Chocolatey <chocolatey@realdimensions.net>
2022-10-18 10:52:41 +0000

    AU: 1 updated - fmedia [skip ci] https://gist.github.com/8a2f24b529db5159a7c189c3b4ccd9a4/8d81633cf503a2d32754c8b5d66ea12c5f3485a0

--- automatic/fmedia/tools/chocolateyInstall.ps1
+++ automatic/fmedia/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'fmedia'
-$url64      = 'https://github.com/stsaz/fmedia/releases/download/v1.29/fmedia-1.29-win-x64.zip'
-$checksum64 = '6afef47da2806056a1ae3c641a27563e0f400098a370bcb89accf738ab55ca28'
+$url64      = 'https://github.com/stsaz/fmedia/releases/download/v1.29.1/fmedia-1.29.1-win-x64.zip'
+$checksum64 = '1ac41fe48ebaf0d96445c4b18e46aa51a637b9c4aef643e3515f1e462a47ec0c'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url64Bit "$url64" `
