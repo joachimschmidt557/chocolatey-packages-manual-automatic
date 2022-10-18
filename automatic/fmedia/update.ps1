a518fa4a4be35cc475e06eeafdeea45f54db5138
joachimschmidt557 <joachim.schmidt557@outlook.com>
2022-10-18 12:50:43 +0200

    fmedia: use GitHub Releases API

--- automatic/fmedia/update.ps1
+++ automatic/fmedia/update.ps1
@@ -1,6 +1,6 @@
 import-module au
 
-$releases = 'https://github.com/stsaz/fmedia/releases'
+$releases = 'https://api.github.com/repos/stsaz/fmedia/releases'
 
 function global:au_SearchReplace {
     @{
@@ -14,21 +14,30 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $response = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $json = $response | ConvertFrom-Json
 
     # fmedia-0.34.1-win-x64.zip
     #$re_32  = "fmedia-.+-win-x64.zip"
     $re_64  = "fmedia-.+-win-x64.zip"
-    #$url32 = $download_page.links | ? href -match $re_32 | select -First 1 -expand href
-    $url64 = $download_page.links | ? href -match $re_64 | select -First 1 -expand href
 
-    #$url32 = "http://fmedia.firmdev.com/" + $url32
-    $url64 = "https://github.com" + $url64
+    foreach ($release in $json) {
+        # $asset32 = $release.assets | ? name -match $re_32
+        $asset64 = $release.assets | ? name -match $re_64
 
-    $version = ($url64 -split '-' | select -last 1 -skip 2) -replace 'beta','-beta'
+        # if ($asset32 -eq $null) { continue }
+        if ($asset64 -eq $null) { continue }
 
-    $Latest = @{ URL64 = $url64; Version = $version }
-    return $Latest
+        # $url32 = $asset32.browser_download_url
+        $url64 = $asset64.browser_download_url
+
+        $version = $release.tag_name -Replace 'v',''
+
+        $Latest = @{ URL64 = $url64; Version = $version }
+        return $Latest
+    }
+
+    throw "No release with suitable binaries found."
 }
 
 update -ChecksumFor 64
