7befd08ecd8f37eca372576f0f3dfeafa741ed81
joachimschmidt557 <joachim.schmidt557@outlook.com>
2022-10-18 12:30:36 +0200

    min.portable: use GitHub Releases API instead of scraping

--- automatic/min.portable/update.ps1
+++ automatic/min.portable/update.ps1
@@ -1,35 +1,44 @@
 import-module au
 
-$releases = 'https://github.com/minbrowser/min/releases'
+$releases = 'https://api.github.com/repos/minbrowser/min/releases'
 
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
             "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            # "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            # "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
             "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
      }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $response = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $json = $response | ConvertFrom-Json
 
     # Min-v1.7.0-win32-ia32.zip
     # Min-v1.7.0-win32-x64.zip
-    # $re_32  = "Min-v[^A-Za-z]+-windows-ia32.zip"
+    $re_32  = "Min-v[^A-Za-z]+-windows-ia32.zip"
     $re_64  = "Min-v[^A-Za-z]+-windows.zip"
-    # $url32 = $download_page.links | ? href -match $re_32 | select -First 1 -expand href
-    $url64 = $download_page.links | ? href -match $re_64 | select -First 1 -expand href
 
-    # $url32 = "https://github.com" + $url32
-    $url64 = "https://github.com" + $url64
+    foreach ($release in $json) {
+        $asset32 = $release.assets | ? name -match $re_32
+        $asset64 = $release.assets | ? name -match $re_64
 
-    $version = ($url64 -split '-' | select -last 1 -skip 2) -Replace 'v',''
+        if ($asset32 -eq $null) { continue }
+        if ($asset64 -eq $null) { continue }
 
-    $Latest = @{ URL64 = $url64; Version = $version }
-    return $Latest
+        $url32 = $asset32.browser_download_url
+        $url64 = $asset64.browser_download_url
+
+        $version = $release.tag_name -Replace 'v',''
+
+        $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+        return $Latest
+    }
+
+    throw "No release with suitable binaries found."
 }
 
-update -ChecksumFor 64
+update
