28b1ab77a205a98716b515dd4c8fb7b0dd972172
Chocolatey <chocolatey@realdimensions.net>
2022-10-26 06:02:10 +0000

    AU: 4 updated - mockoon Shotcut shotcut.install shotcut.portable [skip ci] https://gist.github.com/8a2f24b529db5159a7c189c3b4ccd9a4/c375efd3fd08eac1a15b1a8928a74fa86c0cef2f

--- automatic/shotcut.install/tools/chocolateyInstall.ps1
+++ automatic/shotcut.install/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/mltframework/shotcut/releases/download/v22.10.22/shotcut-win64-221022.exe'
-$checksum64 = 'ac6b4974ce31e3b3a8a1d81dd730cb71ed3743eed8f532d5a5943565ed4343f7'
+$url64      = 'https://github.com/mltframework/shotcut/releases/download/v22.10.25/shotcut-win64-221025.exe'
+$checksum64 = '3f37ac8fcc1571c65915336fdf4e04eae821dfc2f739d7e8bc999506dfaafc40'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
--- automatic/shotcut.portable/tools/chocolateyInstall.ps1
+++ automatic/shotcut.portable/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'shotcut.portable'
-$url64      = 'https://github.com/mltframework/shotcut/releases/download/v22.10.22/shotcut-win64-221022.zip'
-$checksum64 = 'd0527da1847c0f7935a1eb991ccf78ba5264f98897e58c1c65f0304772223a84'
+$url64      = 'https://github.com/mltframework/shotcut/releases/download/v22.10.25/shotcut-win64-221025.zip'
+$checksum64 = '5b7f3ca655bd611b1546f82f631114014215517f39ad35cc9119cdd93305b8d1'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url64Bit "$url64" `
