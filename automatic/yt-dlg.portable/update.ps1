f2cea27cda57d4d565d33104d6f3e9a07fa43ba8
joachimschmidt557 <joachim.schmidt557@outlook.com>
2022-11-16 12:45:39 +0100

    yt-dlg.portable: Use GitHub Releases API

--- automatic/yt-dlg.portable/update.ps1
+++ automatic/yt-dlg.portable/update.ps1
@@ -1,6 +1,6 @@
 import-module au
 
-$releases = 'https://github.com/oleksis/youtube-dl-gui/releases'
+$releases = 'https://api.github.com/api/oleksis/youtube-dl-gui/releases'
 
 function global:au_BeforeUpdate() {
     #Download $Latest.URL32 / $Latest.URL64 in tools directory and remove any older installers.
@@ -17,16 +17,30 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $token = ConvertTo-SecureString $Env:github_api_key -AsPlainText -Force
+    $response = Invoke-WebRequest -Uri $releases -UseBasicParsing -Authentication Bearer -Token $token
+    $json = ConvertFrom-Json $response
 
     # yt-dlg.exe
     $re  = "yt-dlg.exe"
-    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
 
-    $version = ($url -split '/' | select -last 1 -Skip 1) -Replace 'v',''
+    foreach ($release in $json) {
+        $asset32 = $release.assets | ? name -match $re_32
+        # $asset64 = $release.assets | ? name -match $re_64
 
-    $Latest = @{ URL32 = ("https://github.com" + $url); Version = $version }
-    return $Latest
+        if ($asset32 -eq $null) { continue }
+        # if ($asset64 -eq $null) { continue }
+
+        $url32 = $asset32.browser_download_url
+        # $url64 = $asset64.browser_download_url
+
+        $version = $release.tag_name -Replace 'v',''
+
+        $Latest = @{ URL32 = $url32; Version = $version }
+        return $Latest
+    }
+
+    throw "No release with suitable binaries found."
 }
 
 update -ChecksumFor 32
