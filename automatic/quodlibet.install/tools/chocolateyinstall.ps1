c4586ae6132c7ff1e292c6c9d1f80f26fb081ccc
joachimschmidt557 <joachim.schmidt557@outlook.com>
2022-11-30 22:44:20 +0100

    quodlibet.install: Use GitHub Releases API

--- automatic/quodlibet.install/tools/chocolateyinstall.ps1
+++ automatic/quodlibet.install/tools/chocolateyinstall.ps1
@@ -1,22 +1,17 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/quodlibet/quodlibet/releases/download/release-4.5.0/quodlibet-4.5.0-installer.exe'
-$checksum   = 'e6642aaaac95d7746cb4bbc03e4984bdc7f2033d899757ded82e048986ab2f12'
+$fileLocation = Get-Item "$toolsDir\quodlibet-*-installer.msi"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url
+  file          = $fileLocation
 
   softwareName  = 'quod libet*'
-
-  checksum      = $checksum
-  checksumType  = 'sha256'
 
   silentArgs   = '/S'
   validExitCodes= @(0)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
