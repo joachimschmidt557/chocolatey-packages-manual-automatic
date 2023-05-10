1d0ab4aa21928904f42642dbcd17b104124d0a55
joachimschmidt557 <joachim.schmidt557@outlook.com>
2023-05-10 15:11:25 +0200

    add buttercup.portable

---
+++ automatic/buttercup.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# Make a nicer shim
+$longFileName = Get-Item "$toolsDir\Buttercup*-portable.exe" | Select-Object -last 1
+Install-BinFile -Name "buttercup" -Path "$longFileName"
