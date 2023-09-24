2f61a0c00759796c484c8cca25a32b08a821fd44
Chocolatey <chocolatey@realdimensions.net>
2023-09-24 22:13:37 +0000

    AU: 1 updated - windscribe [skip ci] https://gist.github.com/joachimschmidt557/8a2f24b529db5159a7c189c3b4ccd9a4/6bf6d02c887fbde561954aa8cbbfda1d9353b5c9

--- automatic/windscribe/tools/chocolateyinstall.ps1
+++ automatic/windscribe/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://deploy.totallyacdn.com/desktop-apps/2.7.10/Windscribe_2.7.10_guinea_pig.exe'
+$url        = 'https://deploy.totallyacdn.com/desktop-apps/2.7.11/Windscribe_2.7.11_guinea_pig.exe'
 
 Confirm-Win10
 
@@ -13,7 +13,7 @@ $packageArgs = @{
 
   softwareName  = 'windscribe*'
 
-  checksum      = '08e259b29fd9ef73147df1e77f5592b00a4d5a56d240f063f74b164f74292208'
+  checksum      = '539cd1f6fce4de1ee3718526b9ff297f9ff11dff021a39587ac54e7fa4d9acd4'
   checksumType  = 'sha256'
 
   silentArgs   = '-silent'
