fa6d96cd493fc3a1de56dc880c65fcf7161dc117
Chocolatey <chocolatey@realdimensions.net>
2023-10-01 06:02:09 +0000

    AU: 2 updated - Shotcut shotcut.portable [skip ci] https://gist.github.com/joachimschmidt557/8a2f24b529db5159a7c189c3b4ccd9a4/4f42ae4ca8fdc57728a4717b3867e6a735b4279a

--- automatic/shotcut.portable/tools/chocolateyInstall.ps1
+++ automatic/shotcut.portable/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'shotcut.portable'
-$url64      = 'https://github.com/mltframework/shotcut/releases/download/v23.09.12/shotcut-win64-230912.zip'
-$checksum64 = 'ce48b6179ada7e4e5ad9476ccb571866de81958798928a8cc8939e5fb07ea506'
+$url64      = 'https://github.com/mltframework/shotcut/releases/download/v23.09.29/shotcut-win64-230929.zip'
+$checksum64 = '75e394d3b3651239e2b540ea3036fafb7d37259426709ce1f22fb7436a94488e'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url64Bit "$url64" `
