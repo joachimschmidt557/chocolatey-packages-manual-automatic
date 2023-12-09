2c7fef0b8530f25a1259bd3bad5775697a628e73
joachimschmidt557 <joachim.schmidt557@outlook.com>
2023-12-09 09:30:08 +0100

    windscribe: use checksum64 instead of checksum

--- automatic/windscribe/tools/chocolateyinstall.ps1
+++ automatic/windscribe/tools/chocolateyinstall.ps1
@@ -6,18 +6,18 @@ $url        = 'https://deploy.totallyacdn.com/desktop-apps/2.8.5/Windscribe_2.8.
 Confirm-Win10
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url64         = $url
+  packageName     = $env:ChocolateyPackageName
+  unzipLocation   = $toolsDir
+  fileType        = 'EXE'
+  url64           = $url
 
-  softwareName  = 'windscribe*'
+  softwareName    = 'windscribe*'
 
-  checksum      = 'ce813aa33ab7c21a9f918573aa5c3b70ca07dd9a367cef34e0e23f121b9a22d3'
-  checksumType  = 'sha256'
+  checksum64      = 'ce813aa33ab7c21a9f918573aa5c3b70ca07dd9a367cef34e0e23f121b9a22d3'
+  checksumType64  = 'sha256'
 
-  silentArgs   = '-silent'
-  validExitCodes= @(0)
+  silentArgs      = '-silent'
+  validExitCodes  = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
