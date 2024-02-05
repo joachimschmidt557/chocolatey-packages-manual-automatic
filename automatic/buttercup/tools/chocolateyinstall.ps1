2fa575859dcb4662ebb36b0847f90ecf77caf0db
Chocolatey <chocolatey@realdimensions.net>
2024-02-05 06:01:57 +0000

    AU: 2 updated - buttercup buttercup.portable [skip ci] https://gist.github.com/joachimschmidt557/8a2f24b529db5159a7c189c3b4ccd9a4/829ee6ff0e3a35ad39648b36db94bb7ff6e64d2f

--- automatic/buttercup/tools/chocolateyinstall.ps1
+++ automatic/buttercup/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/buttercup/buttercup-desktop/releases/download/v2.24.4/Buttercup-win-x64-2.24.4-installer.exe'
-$checksum64 = '86a264b03f86527b6f266ec9add84a02b916c1cd5bbfe7a4720d94bc51a60d9c'
+$url64      = 'https://github.com/buttercup/buttercup-desktop/releases/download/v2.25.0/Buttercup-win-x64-2.25.0-installer.exe'
+$checksum64 = 'a8c2523d4a4fc5c82a32d1a078874bc2773d5823621e7de5508a32e60edcb604'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
