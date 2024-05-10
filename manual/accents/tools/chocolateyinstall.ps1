87e8bdae3f976519193c3529df3e4edb5066d75f
joachimschmidt557 <joachim.schmidt557@outlook.com>
2024-05-10 21:50:47 +0200

    accents: Fetch fresh download URL from webpage

--- manual/accents/tools/chocolateyinstall.ps1
+++ manual/accents/tools/chocolateyinstall.ps1
@@ -1,5 +1,10 @@
+$download_page = Invoke-WebRequest -Uri "https://www.dcmembers.com/skrommel/download/accents/" -UseBasicParsing
+$link = $download_page.Links | where onclick -match "location.href.*" | select -first 1
+$onclick = $link.onclick
+$url = $onclick -replace "location.href='","" -replace "';return false;",""
+
 Install-ChocolateyZipPackage -PackageName 'Accents' `
-  -Url 'http://www.dcmembers.com/skrommel/download/accents/?wpdmdl=6' `
+  -Url $url `
   -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)" `
-  -Checksum "D6B06BA79E4E67BC5A09E64EACE99E9A91E92F2F691CA7852D761BE26B1FEABF" `
+  -Checksum "8d2113e8a4b27db816f499c962aa87d0f38acf8d7c273c61d0054ce950197a77" `
   -ChecksumType "sha256"
