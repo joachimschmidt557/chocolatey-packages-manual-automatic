61bddeed50aaf0d333c6d3aa4e7d17aad94ce974
joachimschmidt557 <joachim.schmidt557@outlook.com>
2024-09-09 10:50:05 +0200

    [AU resonic.install:0.1]

--- automatic/resonic.install/tools/chocolateyinstall.ps1
+++ automatic/resonic.install/tools/chocolateyinstall.ps1
@@ -4,6 +4,11 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://resonic.at/get/player'
 $checksum   = '53dae7caaaa3568deecff4f29a4d1c67291e3f8cddf47df7930109189bdcb4af'
 
+$options     = @{
+  Headers       = @{
+    'User-Agent' = "User-Agent: Mozilla/5.0 (Windows NT 10.0; rv:129.0) Gecko/20100101 Firefox/129.0"
+  }
+}
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
@@ -14,6 +19,8 @@ $packageArgs = @{
 
   checksum      = $checksum
   checksumType  = 'sha256'
+
+  options       = $options
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
