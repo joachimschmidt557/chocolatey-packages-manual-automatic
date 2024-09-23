6158864e59e25e766baf90b17a28fa74919befa3
joachimschmidt557 <joachim.schmidt557@outlook.com>
2024-09-23 19:35:20 +0200

    add auto update for spek

---
+++ automatic/spek/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileName     = 'spek'
+$fileLocation = Get-Item "$toolsDir\${fileName}"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'MSI'
+
+  softwareName  = 'resonic*'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
---
+++ automatic/spek/update.ps1
@@ -0,0 +1,54 @@
+import-module au
+
+$releases = 'https://api.github.com/repos/alexkay/spek/releases'
+
+function global:au_BeforeUpdate() {
+    #Download $Latest.URL32 / $Latest.URL64 in tools directory and remove any older installers.
+    Get-RemoteFiles -Purge -NoSuffix
+}
+
+function global:au_SearchReplace {
+    @{
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+64-bit:).*"             = "`${1} $($Latest.URL64)"
+            "(?i)(checksum64:).*"           = "`${1} $($Latest.Checksum64)"
+          }
+
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]version\s*=\s*)('.*')"      = "`$1'$($Latest.FileName)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $token = ConvertTo-SecureString $Env:github_api_key -AsPlainText -Force
+    $response = Invoke-WebRequest -Uri $releases -UseBasicParsing -Authentication Bearer -Token $token
+    $json = ConvertFrom-Json $response
+
+    # spek-0.8.5-beta.msi
+    $re_64  = "^spek-.+.msi$"
+
+    foreach ($release in $json) {
+        # $asset32 = $release.assets | ? name -match $re_32
+        $asset64 = $release.assets | ? name -match $re_64
+
+        # if ($asset32 -eq $null) { continue }
+        if ($asset64 -eq $null) { continue }
+
+        # $url32 = $asset32.browser_download_url
+        $url64 = $asset64.browser_download_url
+
+        $version = $release.tag_name -Replace 'v',''
+
+        if ($release.prerelease) {
+            $version = $version + "-pre"
+        }
+
+        $Latest = @{ URL64 = $url64; Version = $version; FileName = $asset64.name }
+        return $Latest
+    }
+
+    throw "No release with suitable binaries found."
+}
+
+update -ChecksumFor None
