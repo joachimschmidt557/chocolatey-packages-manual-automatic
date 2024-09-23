08900470a9a2c0cb1583ce41b77657fb22b8cd9e
Chocolatey <chocolatey@realdimensions.net>
2024-09-23 12:09:54 +0000

    AU: 1 updated - buttercup.install [skip ci] https://gist.github.com/joachimschmidt557/8a2f24b529db5159a7c189c3b4ccd9a4/8fd1fab3c05c6469676c4af8c9b968b78c092818

--- automatic/buttercup.install/tools/chocolateyinstall.ps1
+++ automatic/buttercup.install/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$version      = '2.28.0'
+$version      = '2.28.1'
 $fileLocation = Get-Item "$toolsDir\Buttercup-win-x64-${version}-installer.exe"
 
 $packageArgs = @{
