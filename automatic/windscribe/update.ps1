acf7c472ac37308393e9fa32d91ea50905ffe562
joachimschmidt557 <joachim.schmidt557@outlook.com>
2024-11-19 23:03:27 +0100

    windscribe: use GitHub API

--- automatic/windscribe/update.ps1
+++ automatic/windscribe/update.ps1
@@ -1,5 +1,7 @@
 Import-Module au
 
+$releases = 'https://api.github.com/repos/Windscribe/Desktop-App/releases'
+
 function global:au_BeforeUpdate {
   $Latest.Checksum64 = Get-RemoteChecksum -Url $Latest.Url64 -Algorithm sha256
 }
@@ -17,38 +19,85 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-  $changelogUrl = 'https://windscribe.com/changelog/windows'
-  $userAgent = 'Update checker of Chocolatey Community Package ''windscribe'''
-  $changelogPage = Invoke-WebRequest -Uri $changelogUrl -UserAgent $userAgent -UseBasicParsing
+    $token = ConvertTo-SecureString $Env:github_api_key -AsPlainText -Force
+    $response = Invoke-WebRequest -Uri $releases -UseBasicParsing -Authentication Bearer -Token $token
+    $json = ConvertFrom-Json $response
 
-  if ($changelogPage.Content -notmatch '> v([\d\.]+)</a> \(Alpha\) </h3>')
-  {
-    throw 'Alpha version not found!'
-  }
-  $alphaVersion = $Matches[1]
-  $alphaUrl = $changelogPage.Links | Where-Object href -like "*/$alphaVersion/*" | Select-Object -First 1 -Expand href
+    # Release: Windscribe_2.11.11.exe
+    $re_release     = "^Windscribe_[^A-Za-z]+.exe$"
+    $release_data   = $null
 
-  if ($changelogPage.Content -notmatch '> v([\d\.]+)</a> \(Beta\) </h3>')
-  {
-    throw 'Beta version not found!'
-  }
-  $betaVersion = $Matches[1]
-  $betaUrl = $changelogPage.Links | Where-Object href -like "*/$betaVersion/*" | Select-Object -First 1 -Expand href
+    # Beta: Windscribe_2.12.4_beta.exe
+    $re_beta     = "^Windscribe_[^A-Za-z]+_beta.exe$"
+    $beta_data   = $null
 
-  if ($changelogPage.Content -notmatch '> v([\d\.]+)</a> </h3>')
-  {
-    throw 'Stable version not found!'
-  }
-  $stableVersion = $Matches[1]
-  $stableUrl = $changelogPage.Links | Where-Object href -like "*/$stableVersion/*" | Select-Object -First 1 -Expand href
+    # Alpha: Windscribe_2.12.1_guinea_pig.exe
+    $re_alpha     = "^Windscribe_[^A-Za-z]+_gunea_pig.exe$"
+    $alpha_data   = $null
 
-  $streams = [ordered] @{
-    Alpha = @{ Url64 = $alphaUrl; Version = "$alphaVersion-alpha" }
-    Beta = @{ Url64 = $betaUrl; Version = "$betaVersion-beta" }
-    Stable = @{ Url64 = $stableUrl; Version = $stableVersion }
-  }
+    foreach ($release in $json) {
+        $asset64 = $release.assets | ? name -match $re_release
 
-  return @{ Streams = $streams }
+        if ($asset64 -eq $null) { continue }
+
+        $url64 = $asset64.browser_download_url
+
+        $version = $release.tag_name -Replace 'v',''
+
+        $release_data = @{ URL64 = $url64; Version = $version }
+
+        break
+    }
+
+    foreach ($release in $json) {
+        $asset64 = $release.assets | ? name -match $re_beta
+
+        if ($asset64 -eq $null) { continue }
+
+        $url64 = $asset64.browser_download_url
+
+        $version = $release.tag_name -Replace 'v',''
+        $version = ($version.split(".") | select -first 3) -join '.'
+
+        $beta_data = @{ URL64 = $url64; Version = $version }
+
+        break
+    }
+
+    foreach ($release in $json) {
+        $asset64 = $release.assets | ? name -match $re_alpha
+
+        if ($asset64 -eq $null) { continue }
+
+        $url64 = $asset64.browser_download_url
+
+        $version = $release.tag_name -Replace 'v',''
+        $version = ($version.split(".") | select -first 3) -join '.'
+
+        $alpha_data = @{ URL64 = $url64; Version = $version }
+
+        break
+    }
+
+    if ($release_data -eq $null) {
+        throw "No release with suitable binaries found."
+    }
+
+    if ($beta_data -eq $null) {
+        throw "No beta release with suitable binaries found."
+    }
+
+    if ($alpha_data -eq $null) {
+        throw "No alpha release with suitable binaries found."
+    }
+
+    $streams = [ordered] @{
+        Alpha = $alpha_data
+        Beta = $beta_data
+        Stable = $release_data
+    }
+
+    return @{ Streams = $streams }
 }
 
 Update-Package -ChecksumFor None -NoReadme
