8507279a5b49580e5809d292e3461a471f73b97e
joachimschmidt557 <joachim.schmidt557@outlook.com>
2025-03-16 19:30:31 +0100

    godot: use github releases api

--- automatic/godot/update.ps1
+++ automatic/godot/update.ps1
@@ -1,6 +1,6 @@
 import-module au
 
-$releases = 'https://godotengine.org/download/windows'
+$releases = 'https://api.github.com/repos/godotengine/godot-builds/releases'
 
 function global:au_BeforeUpdate() {
     #Download $Latest.URL32 / $Latest.URL64 in tools directory and remove any older installers.
@@ -19,22 +19,33 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $token = ConvertTo-SecureString $Env:github_api_key -AsPlainText -Force
+    $response = Invoke-WebRequest -Uri $releases -UseBasicParsing -Authentication Bearer -Token $token
+    $json = ConvertFrom-Json $response
 
     # Godot_v3.0.6-stable_win64.exe.zip
     # Godot_v3.0.6-stable_win32.exe.zip
-    $re_32  = "Godot_v.+-stable_win32.exe.zip"
-    $re_64  = "Godot_v.+-stable_win64.exe.zip"
-    $url32 = ($download_page.links | ? href -match $re_32 | select -First 1 -expand href).Trim()
-    $url64 = ($download_page.links | ? href -match $re_64 | select -First 1 -expand href).Trim()
+    # Godot_v4.4.1-rc1_win32.exe.zip
+    $re_32  = "Godot_v.+_win32.exe.zip"
+    $re_64  = "Godot_v.+_win64.exe.zip"
 
-    #$url32 = "https://github.com" + $url32
-    #$url64 = "https://github.com" + $url64
+    foreach ($release in $json) {
+        $asset32 = $release.assets | ? name -match $re_32
+        $asset64 = $release.assets | ? name -match $re_64
 
-    $version = ($url32 -split '/' | select -last 1 -skip 1) -Replace '-stable',''
+        if ($asset32 -eq $null) { continue }
+        if ($asset64 -eq $null) { continue }
 
-    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
-    return $Latest
+        $url32 = $asset32.browser_download_url
+        $url64 = $asset64.browser_download_url
+
+        $version = $release.tag_name -Replace '-stable',''
+
+        $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+        return $Latest
+    }
+
+    throw "No release with suitable binaries found."
 }
 
 update -ChecksumFor none
