56a99349cd0977140bf5ea50a034b07b5db909fa
Chocolatey <chocolatey@realdimensions.net>
2025-03-21 15:34:44 +0000

    AU: 2 updated - godot-mono open-stage-control [skip ci] https://gist.github.com/joachimschmidt557/8a2f24b529db5159a7c189c3b4ccd9a4/0882ba867a876eae0c0ad3adae62e5eb032a544b

--- automatic/godot-mono/tools/chocolateyInstall.ps1
+++ automatic/godot-mono/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileName32 = 'Godot_v4.5-dev1_win32.exe_x32.zip'
-$fileName64 = 'Godot_v4.5-dev1_win64.exe_x64.zip'
+$fileName32 = 'Godot_v4.5-dev1_mono_win32_x32.zip'
+$fileName64 = 'Godot_v4.5-dev1_mono_win64_x64.zip'
 $zipFile    = Get-Item "$toolsDir\${fileName32}.zip"
 $zipFile64  = Get-Item "$toolsDir\${fileName64}.zip"
 
