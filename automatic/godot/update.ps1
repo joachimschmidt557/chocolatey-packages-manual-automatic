4bdfa63b7e4fd26cd5af576b5caaefb882b4498b
joachimschmidt557 <joachim.schmidt557@outlook.com>
2025-03-26 17:41:29 +0100

    godot: use version streams

--- automatic/godot/update.ps1
+++ automatic/godot/update.ps1
@@ -1,6 +1,6 @@
 import-module au
 
-$releases = 'https://api.github.com/repos/godotengine/godot-builds/releases'
+$releases = 'https://api.github.com/repos/godotengine/godot-builds/releases?per_page=60'
 
 function global:au_BeforeUpdate() {
     #Download $Latest.URL32 / $Latest.URL64 in tools directory and remove any older installers.
@@ -28,29 +28,49 @@ function global:au_GetLatest {
     $response = Invoke-WebRequest -Uri $releases -UseBasicParsing -Authentication Bearer -Token $token
     $json = ConvertFrom-Json $response
 
+    # see https://docs.godotengine.org/en/stable/about/release_policy.html
+    # TODO omitted 3.7 for now as no corresponding release exists yet
+    $supported_version_families = @("3.5", "3.6", "4.2", "4.3", "4.4", "4.5")
+
     # Godot_v3.0.6-stable_win64.exe.zip
     # Godot_v3.0.6-stable_win32.exe.zip
     # Godot_v4.4.1-rc1_win32.exe.zip
     $re_32  = "Godot_v.+_win32.exe.zip"
     $re_64  = "Godot_v.+_win64.exe.zip"
 
-    foreach ($release in $json) {
-        $asset32 = $release.assets | ? name -match $re_32
-        $asset64 = $release.assets | ? name -match $re_64
+    $Streams = [ordered] @{}
 
-        if ($asset32 -eq $null) { continue }
-        if ($asset64 -eq $null) { continue }
+    foreach ($version_family in $supported_version_families) {
+        $data = $null
 
-        $url32 = $asset32.browser_download_url
-        $url64 = $asset64.browser_download_url
+        foreach ($release in $json) {
+            if (!$release.tag_name.StartsWith($version_family)) { continue }
 
-        $version = $release.tag_name -Replace '-stable',''
+            $asset32 = $release.assets | ? name -match $re_32
+            $asset64 = $release.assets | ? name -match $re_64
 
-        $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
-        return $Latest
+            if ($asset32 -eq $null) { continue }
+            if ($asset64 -eq $null) { continue }
+
+            $url32 = $asset32.browser_download_url
+            $url64 = $asset64.browser_download_url
+
+            $version = $release.tag_name -Replace '-stable',''
+
+            $data = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+            break
+        }
+
+        if ($data -eq $null) {
+            throw "No release with suitable binaries found for version family $version_family."
+        }
+
+        $Streams.Add($version_family, $data)
     }
 
-    throw "No release with suitable binaries found."
+    @{
+        Streams = $Streams
+    }
 }
 
 update -ChecksumFor none
