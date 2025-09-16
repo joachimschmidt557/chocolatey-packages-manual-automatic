ac2e8f1c1cfa0a7efaa7ce79c0eaf8b6195587d2
Chocolatey <chocolatey@realdimensions.net>
2025-09-16 06:03:16 +0000

    AU: 2 updated - godot windscribe [skip ci] https://gist.github.com/joachimschmidt557/8a2f24b529db5159a7c189c3b4ccd9a4/8619355eb304be05f0706df103ed8eb6b3f21556

--- automatic/godot/tools/chocolateyInstall.ps1
+++ automatic/godot/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileName32 = 'Godot_v4.5-rc2_win32.exe_x32.zip'
-$fileName64 = 'Godot_v4.5-rc2_win64.exe_x64.zip'
+$fileName32 = 'Godot_v4.5-stable_win32.exe_x32.zip'
+$fileName64 = 'Godot_v4.5-stable_win64.exe_x64.zip'
 $zipFile    = Get-Item "$toolsDir\${fileName32}"
 $zipFile64  = Get-Item "$toolsDir\${fileName64}"
 
--- automatic/windscribe/tools/chocolateyinstall.ps1
+++ automatic/windscribe/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/Windscribe/Desktop-App/releases/download/v2.17.4/Windscribe_2.17.4_guinea_pig_amd64.exe'
+$url        = 'https://github.com/Windscribe/Desktop-App/releases/download/v2.17.5/Windscribe_2.17.5_beta_amd64.exe'
 
 Confirm-Win10
 
@@ -13,7 +13,7 @@ $packageArgs = @{
 
   softwareName    = 'windscribe*'
 
-  checksum64      = '45511895edd338c8f6aca2c825423d0e435e70ebc37e1e58d54a178646fb2c22'
+  checksum64      = '0f0763c740cdfde3066423e427fa45092e31f93980811558b18604fce8b5b382'
   checksumType64  = 'sha256'
 
   silentArgs      = '-silent'
