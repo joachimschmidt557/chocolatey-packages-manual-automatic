8bcbd26cbd815dad0e96acc85def4df98b90c281
Chocolatey <chocolatey@realdimensions.net>
2026-01-12 06:02:52 +0000

    AU: 1 updated - phiola.portable [skip ci] https://gist.github.com/joachimschmidt557/8a2f24b529db5159a7c189c3b4ccd9a4/39fc0a75df36b4594bdf68f374e7787b44861f42

--- automatic/phiola.portable/tools/chocolateyInstall.ps1
+++ automatic/phiola.portable/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileName   = 'phiola-2.6.8-windows-x64.zip'
+$fileName   = 'phiola-2.7-beta0-windows-x64.zip'
 $zipFile64  = Get-Item "$toolsDir\${fileName}"
 
 $packageArgs = @{
