9a124d65e9567e9587efda2f9c3a5fcce1018079
Chocolatey <chocolatey@realdimensions.net>
2026-03-09 06:02:31 +0000

    AU: 1 updated - fd [skip ci] https://gist.github.com/joachimschmidt557/8a2f24b529db5159a7c189c3b4ccd9a4/38a37ce06715a91bd1c70a0b49a00b833c23065c

--- automatic/fd/tools/chocolateyInstall.ps1
+++ automatic/fd/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileName32 = 'Godot_v4.4.1-stable_win32.exe_x32.zip'
-$fileName64 = 'Godot_v4.4.1-stable_win64.exe_x64.zip'
+$fileName32 = 'fd-v10.4.1-i686-pc-windows-msvc_x32.zip'
+$fileName64 = 'fd-v10.4.1-x86_64-pc-windows-msvc_x64.zip'
 $zipFile    = Get-Item "$toolsDir\${fileName32}"
 $zipFile64  = Get-Item "$toolsDir\${fileName64}"
 
