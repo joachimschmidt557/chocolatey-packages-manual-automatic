0584fc8b72e2fe169fa0ef289275a57df53dba74
Chocolatey <chocolatey@realdimensions.net>
2026-03-10 06:03:41 +0000

    AU: 3 updated - godot godot-mono windscribe [skip ci] https://gist.github.com/joachimschmidt557/8a2f24b529db5159a7c189c3b4ccd9a4/014ba3667fc297ddc7dda2f541f0c3326e49527e

--- automatic/godot-mono/tools/chocolateyInstall.ps1
+++ automatic/godot-mono/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32 = 'https://github.com/godotengine/godot-builds/releases/download/4.7-dev2/Godot_v4.7-dev2_mono_win32.zip'
-$url64 = 'https://github.com/godotengine/godot-builds/releases/download/4.7-dev2/Godot_v4.7-dev2_mono_win64.zip'
-$checksum32   = 'fa1c57f4bbc24ab0d2982cf5671cc6f408de560e30f4a4bab6cf10cd22d4775c'
-$checksum64   = 'a3440e7c956c5b88d802563fdd1e4a685600cace86dd5065e4a7b9260a4cb917'
+$url32 = 'https://github.com/godotengine/godot-builds/releases/download/4.6.2-rc1/Godot_v4.6.2-rc1_mono_win32.zip'
+$url64 = 'https://github.com/godotengine/godot-builds/releases/download/4.6.2-rc1/Godot_v4.6.2-rc1_mono_win64.zip'
+$checksum32   = '4e71aaa6900b11ea1f2d658fec5d46bd0aac3c2c05ecb40c3f803474c7649d7e'
+$checksum64   = '940a2332707c8c73356d1b9b78281f85cba1f0c485d04ee75ffba97d921b5491'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
--- automatic/windscribe/tools/chocolateyinstall.ps1
+++ automatic/windscribe/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/Windscribe/Desktop-App/releases/download/v2.21.3/Windscribe_2.21.3_guinea_pig_amd64.exe'
+$url        = 'https://github.com/Windscribe/Desktop-App/releases/download/v2.21.4/Windscribe_2.21.4_guinea_pig_amd64.exe'
 
 Confirm-Win10
 
@@ -13,7 +13,7 @@ $packageArgs = @{
 
   softwareName    = 'windscribe*'
 
-  checksum64      = '153ce541da717025281317b39ae99dfd656b818ad689ad8f725a02b27ee16bbb'
+  checksum64      = 'd4ffe0275f12e0358e2ab536395fc2e3002f34a4f0fd6fb26b6d627bb621021c'
   checksumType64  = 'sha256'
 
   silentArgs      = '-silent'
