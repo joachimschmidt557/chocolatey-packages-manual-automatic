0ac3170ea474601f98ee1deef837f9d19af78e02
Chocolatey <chocolatey@realdimensions.net>
2026-03-11 06:04:17 +0000

    AU: 1 updated - fd [skip ci] https://gist.github.com/joachimschmidt557/8a2f24b529db5159a7c189c3b4ccd9a4/b3b32067117d9829088c3cf9fc55dd3a238d19d0

--- automatic/fd/tools/chocolateyInstall.ps1
+++ automatic/fd/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileName32 = 'fd-v10.4.1-i686-pc-windows-msvc_x32.zip'
-$fileName64 = 'fd-v10.4.1-x86_64-pc-windows-msvc_x64.zip'
+$fileName32 = 'fd-v10.4.2-i686-pc-windows-msvc_x32.zip'
+$fileName64 = 'fd-v10.4.2-x86_64-pc-windows-msvc_x64.zip'
 $zipFile    = Get-Item "$toolsDir\${fileName32}"
 $zipFile64  = Get-Item "$toolsDir\${fileName64}"
 
