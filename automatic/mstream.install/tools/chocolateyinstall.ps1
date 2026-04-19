db5cfa4e8a807518490725fad9fb647156bced1e
Chocolatey <chocolatey@realdimensions.net>
2026-04-19 06:02:52 +0000

    AU: 2 updated - mstream mstream.install [skip ci] https://gist.github.com/joachimschmidt557/8a2f24b529db5159a7c189c3b4ccd9a4/096d32768db4012b3abe3f5f076388ec00cb4811

--- automatic/mstream.install/tools/chocolateyinstall.ps1
+++ automatic/mstream.install/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/IrosTheBeggar/mStream/releases/download/v6.2.0/mStream-Server-Setup-6.2.0.exe'
-$checksum   = 'fb1fbd4b420d5e0d94f669ad6bec60fbda97f2f35ffcf022a5810f8a1abfc53e'
+$url        = 'https://github.com/IrosTheBeggar/mStream/releases/download/v6.3.0/mStream-Server-Setup-6.3.0.exe'
+$checksum   = '1da4b55b6d5571c75ff94e81dfce55e05934b460789376dd27bc9e8ec5bfd323'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
