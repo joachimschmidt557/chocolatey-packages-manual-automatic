ea75afd50beed2a62b5a03069ab384a222d940d0
Chocolatey <chocolatey@realdimensions.net>
2026-05-09 06:06:31 +0000

    AU: 1 updated - godot-mono [skip ci] https://gist.github.com/joachimschmidt557/8a2f24b529db5159a7c189c3b4ccd9a4/c422329e48db4687f15322dc1d3b8aa2bde233fa

--- automatic/godot-mono/tools/chocolateyInstall.ps1
+++ automatic/godot-mono/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32 = 'https://github.com/godotengine/godot-builds/releases/download/4.7-dev5/Godot_v4.7-dev5_mono_win32.zip'
-$url64 = 'https://github.com/godotengine/godot-builds/releases/download/4.7-dev5/Godot_v4.7-dev5_mono_win64.zip'
-$checksum32   = 'd1a4d7ead36dbead6a88d20c5266e586ceba450bde48d626915d9be8f3d3fafb'
-$checksum64   = '7f9cb0dd364341e7932b0163acb36d29ff87f8c0e9b72230f888e189decb1cfe'
+$url32 = 'https://github.com/godotengine/godot-builds/releases/download/4.6.3-rc1/Godot_v4.6.3-rc1_mono_win32.zip'
+$url64 = 'https://github.com/godotengine/godot-builds/releases/download/4.6.3-rc1/Godot_v4.6.3-rc1_mono_win64.zip'
+$checksum32   = 'c71758d457c0913f3f2495012348f54f5cd0d35a79be299010a114451caacac6'
+$checksum64   = '45c141c5d735076d80d4835de42f9b97c98004fec7c0ce81e1b37c034223891b'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
