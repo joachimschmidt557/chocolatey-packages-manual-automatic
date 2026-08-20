bb3f9ef98628e514a9762b59f9330275e54336ca
Chocolatey <chocolatey@realdimensions.net>
2026-08-20 06:04:30 +0000

    AU: 1 updated - windscribe [skip ci] https://gist.github.com/joachimschmidt557/8a2f24b529db5159a7c189c3b4ccd9a4/14f972e187617a671a4d5ccb8be9a1e511deb586

--- automatic/windscribe/tools/chocolateyinstall.ps1
+++ automatic/windscribe/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/Windscribe/Desktop-App/releases/download/v2.24.9/Windscribe_2.24.9_beta_amd64.exe'
+$url        = 'https://github.com/Windscribe/Desktop-App/releases/download/v2.24.10/Windscribe_2.24.10_beta_amd64.exe'
 
 Confirm-Win10
 
@@ -13,7 +13,7 @@ $packageArgs = @{
 
   softwareName    = 'windscribe*'
 
-  checksum64      = 'db7da5adb49689a9714cabc2c77e8d97fab83c0baf6add6ffda63450f4e900b8'
+  checksum64      = '14a4e44176be3fd2198be468df20979bedb95d396af0990d6e44c8fbb0e28a17'
   checksumType64  = 'sha256'
 
   silentArgs      = '-silent'
