12fac4d57d4878e37a30772c821dfcd085581933
Chocolatey <chocolatey@realdimensions.net>
2026-08-31 06:07:02 +0000

    AU: 3 updated - open-stage-control phiola.portable qpdf [skip ci] https://gist.github.com/joachimschmidt557/8a2f24b529db5159a7c189c3b4ccd9a4/1eee12fd7ba4498c587ca5880eb60011f4433678

--- automatic/qpdf/tools/chocolateyInstall.ps1
+++ automatic/qpdf/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileName32 = 'qpdf-12.4.0-mingw32_x32.zip'
-$fileName64 = 'qpdf-12.4.0-mingw64_x64.zip'
+$fileName32 = 'qpdf-12.4.1-mingw32_x32.zip'
+$fileName64 = 'qpdf-12.4.1-mingw64_x64.zip'
 $zipFile    = Get-Item "$toolsDir\${fileName32}"
 $zipFile64  = Get-Item "$toolsDir\${fileName64}"
 
